a2cb3ee5415eac6a4e35a784af9a3c1563e98c2e
Pedro Celestino <pedrojuniorcelestinocg@gmail.com>
2025-02-12 17:06:38 -0400

    feat-perfil component/ falta margin

---
+++ src/views/components/perfil.component.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./../../styles/global.css">
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+
+  </head>
+  <body class="bg-[#000208]">
+    <div class="flex items-center">
+      <img class="bg-blue-500 size-14.5 rounded-2xl" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSXWxPsJwNMj8brX72-MA5-QyTdZp4lDJ6Zw&s" alt="">
+      <div name="content" class="">
+        <div class="ml-100">
+            <h3 class="text-white text-base font-semibold">Anitta</h3>
+            <p class="text-[#C4C4C4] font-medium text-sm">40m de seguidores</p>
+        </div>
+        <div class="bg-[#202024]  rounded-2xl w-25 text-center h-4 flex items-center justify-center">
+            <p class="text-[0.62rem] text-[#C4C4C4]"> # Tecnologia</p>
+        </div>
+      </div>
+    </div>
+  </body>
+</html>
