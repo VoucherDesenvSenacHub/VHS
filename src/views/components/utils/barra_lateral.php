580559f3ad6fb332cd6de844dd63e47083cd8c15
Kayke Adriano <kayke.adrianobdg@gmail.com>
2025-02-12 17:12:45 -0400

    barra

---
+++ src/views/components/utils/barra_lateral.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Barra Lateral</title>
+</head>
+<body class="bg-gray-900 text-white">
+    <aside class="w-64 h-screen bg-gray-950 p-5">
+            <h2 class="text-gray-400 text-xs font-semibold mb-2"> </h2>
+
+            <ul class="mb-4">
+                <li class="flex items-center gap-3 text-gray-300 py-2 px-3 rounded-lg hover:bg-gray-800 cursor-pointer">
+
+                <svg width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M5.09311 12.8493V10.8108C5.09311 10.2905 5.51804 9.86861 6.04221 9.86861L7.95833 9.86861C8.21005 9.86861 8.45146 9.96788 8.62945 10.1446C8.80744 10.3213 8.90744 10.5609 8.90744 10.8108V12.8493C8.90584 13.0656 8.9913 13.2737 9.14484 13.4272C9.29837 13.5807 9.50729 13.6671 9.72522 13.6671H11.0325C11.643 13.6686 12.2291 13.429 12.6614 13.0009C13.0936 12.5729 13.3366 11.9917 13.3366 11.3856V5.57831C13.3366 5.08871 13.118 4.6243 12.7397 4.31019L8.29261 0.78432C7.51903 0.166115 6.41067 0.186075 5.66018 0.831726L1.3146 4.31019C0.918416 4.61504 0.681623 5.08083 0.669922 5.57831V11.3797C0.669922 12.643 1.70151 13.6671 2.97404 13.6671H4.25145C4.70407 13.6671 5.07192 13.3046 5.0752 12.8552L5.09311 12.8493Z" fill="white"/>
+                </svg> Inicio
+
+                
+            </li>
+
+        </ul>
+
+    </aside>
+</body>
+</html>
