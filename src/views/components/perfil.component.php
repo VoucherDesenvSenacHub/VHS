b08ec09510236527f42764745f64a38cba62bb63
Pedro Celestino <pedrojuniorcelestinocg@gmail.com>
2025-02-13 16:05:48 -0400

    perfil component pronto

--- src/views/components/perfil.component.php
+++ src/views/components/perfil.component.php
@@ -1,24 +1,19 @@
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="./../../styles/global.css">
-    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
-
-  </head>
-  <body class="bg-[#000208]">
-    <div class="flex items-center">
-      <img class="bg-blue-500 size-14.5 rounded-2xl" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRSXWxPsJwNMj8brX72-MA5-QyTdZp4lDJ6Zw&s" alt="">
-      <div name="content" class="">
-        <div class="ml-100">
-            <h3 class="text-white text-base font-semibold">Anitta</h3>
-            <p class="text-[#C4C4C4] font-medium text-sm">40m de seguidores</p>
-        </div>
-        <div class="bg-[#202024]  rounded-2xl w-25 text-center h-4 flex items-center justify-center">
-            <p class="text-[0.62rem] text-[#C4C4C4]"> # Tecnologia</p>
-        </div>
+<?php
+namespace Src\Views\Components;
+function perfilComponent($photo_profile,$creator_name,$subscribers,$category){
+  return " 
+    <div class='flex items-center'>
+      <img class='bg-blue-500 size-14.5 rounded-2xl' src='$photo_profile' alt=''>
+      <div name='content' class=''>
+        <div class='relative left-2.5'>
+          <h3 class='text-white text-base font-semibold'> $creator_name </h3>
+          <p class='text-[#C4C4C4] font-medium text-sm'>$subscribers de seguidores</p>
       </div>
-    </div>
-  </body>
-</html>
+      <div class='bg-[#202024]  rounded-2xl w-25 text-center h-4 flex items-center justify-center relative left-2'>
+          <p class='text-[0.62rem] text-[#C4C4C4]'> $category</p>
+      </div>
+      </div>
+    </div>";
+
+}
+?>
