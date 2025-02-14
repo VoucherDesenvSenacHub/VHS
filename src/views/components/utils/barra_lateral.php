e096d163bbb2d306e7d2f9639b4693f966d37036
Kayke Adriano <kayke.adrianobdg@gmail.com>
2025-02-14 17:10:48 -0400

    barraaaa

--- src/views/components/utils/barra_lateral.php
+++ src/views/components/utils/barra_lateral.php
@@ -1,31 +1,98 @@
+
 <!DOCTYPE html>
-<html lang="pt-Br">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <script src="https://cdn.tailwindcss.com"></script>
     <title>Barra Lateral</title>
 </head>
-<body class="bg-gray-900 text-white">
-    <aside class="w-[9.25rem] h-screen bg-gray-950 p-5" >
-        <h2 class="text-gray-400 text-xs font-semibold mb-2">Home</h2>
+<body >
+    <aside class="w-[11rem] h-screen bg-gray-950 p-5">
+            <h2 class="text-gray-400 text-xs font-semibold mb-2">Home</h2>
  
-        <ul class="mb-4">
-            <li class="flex items-center gap-3 text-gray-300 py-2 px-3 rounded-lg hover:bg-gray-800 cursor-pointer ">
-                <div>
-                    <img src="../../../../public/icons/Home.svg" alt="">
-                </div> Home
-            </li> 
-            
-            <li class="flex items-center gap-3 text-gray-300 py-2 px-3 rounded-lg hover:bg-gray-800 cursor-pointer ">
-                <div>
-                    <img src="../../../../public/icons/Home.svg" alt="">
-                </div> Home
-            </li> 
+            <ul class="mb-4">
 
+                <li class="flex items-center  text-gray-300 rounded-lg hover:bg-gray-800 cursor-pointer mt-[1.5rem]">
+                    <div class="w-[2rem] h-[2rem] flex items-center justify-center bg-opacity-10  bg-white rounded-[12px] ml-[0.31rem] ">
+                        <div class="w-[1rem] h-[1rem] flex items-center justify-center">
+                            <img  src="../../../../public/icons/Home.svg" alt="">
+                        </div>
+                    </div>
+                    <h2 class=" ml-[1rem] text-gray-400 text-sm font-semibold">Inicio</h2>
+                </li>
 
-        </ul>
-   
+                <li class="flex items-center  text-gray-300 rounded-lg hover:bg-gray-800 cursor-pointer mt-[1.5rem]">
+                    <div class="w-[2rem] h-[2rem] flex items-center justify-center bg-opacity-10  bg-white rounded-[12px] ml-[0.31rem] ">
+                        <div class="w-[1rem] h-[1rem] flex items-center justify-center">
+                            <img src="../../../../public/icons/Fast.svg" alt="">
+                        </div>
+                    </div>
+                    <h2 class=" ml-[1rem] text-gray-400 text-sm font-semibold">Fast</h2>
+                </li>
+
+                <li class="flex items-center  text-gray-300 rounded-lg hover:bg-gray-800 cursor-pointer mt-[1.5rem]">
+                    <div class="w-[2rem] h-[2rem] flex items-center justify-center bg-opacity-10  bg-white rounded-[12px] ml-[0.31rem] ">
+                        <div class="w-[1rem] h-[1rem] flex items-center justify-center">
+                            <img src="../../../../public/icons/Eventos.svg" alt="">
+                        </div>
+                    </div>
+                    <h2 class=" ml-[1rem] text-gray-400 text-sm font-semibold">Eventos</h2>
+                </li>
+
+                <li class="flex items-center  text-gray-300 rounded-lg hover:bg-gray-800 cursor-pointer mt-[1.5rem]">
+                    <div class="w-[2rem] h-[2rem] flex items-center justify-center bg-opacity-10  bg-white rounded-[12px] ml-[0.31rem] ">
+                        <div class="w-[1rem] h-[1rem] flex items-center justify-center">
+                            <img src="../../../../public/icons/Historico.svg" alt="">
+                        </div>
+                    </div>
+                    <h2 class=" ml-[1rem] text-gray-400 text-sm font-semibold">Historico</h2>
+                </li>
+                
+            </ul>
+
+            <h2 class="text-gray-400 text-xs font-semibold mb-2">Categoria</h2>
+ 
+            <ul class="mb-4">
+
+                <li class="flex items-center  text-gray-300 rounded-lg hover:bg-gray-800 cursor-pointer mt-[1.5rem]">
+                    <div class="w-[2rem] h-[2rem] flex items-center justify-center bg-opacity-10  bg-white rounded-[12px] ml-[0.31rem] ">
+                        <div class="w-[1rem] h-[1rem] flex items-center justify-center">
+                            <img src="../../../../public/icons/Tecnologia.svg" alt="">
+                        </div>
+                    </div>
+                    <h2 class=" ml-[1rem] text-gray-400 text-sm font-semibold">Tecnologia</h2>
+                </li>
+
+                <li class="flex items-center  text-gray-300 rounded-lg hover:bg-gray-800 cursor-pointer mt-[1.5rem]">
+                    <div class="w-[2rem] h-[2rem] flex items-center justify-center bg-opacity-10  bg-white rounded-[12px] ml-[0.31rem] ">
+                        <div class="w-[1rem] h-[1rem] flex items-center justify-center">
+                            <img src="../../../../public/icons/Saude.svg" alt="">
+                        </div>
+                    </div>
+                    <h2 class=" ml-[1rem] text-gray-400 text-sm font-semibold">Saúde</h2>
+                </li>
+
+                <li class="flex items-center  text-gray-300 rounded-lg hover:bg-gray-800 cursor-pointer mt-[1.5rem]">
+                    <div class="w-[2rem] h-[2rem] flex items-center justify-center bg-opacity-10  bg-white rounded-[12px] ml-[0.31rem] ">
+                        <div class="w-[1rem] h-[1rem] flex items-center justify-center">
+                            <img src="../../../../public/icons/Moda.svg" alt="">
+                        </div>
+                    </div>
+                    <h2 class=" ml-[1rem] text-gray-400 text-sm font-semibold">Moda</h2>
+                </li>
+
+                <li class="flex items-center  text-gray-300 rounded-lg hover:bg-gray-800 cursor-pointer mt-[1.5rem]">
+                    <div class="w-[2rem] h-[2rem] flex items-center justify-center bg-opacity-10  bg-white rounded-[12px] ml-[0.31rem] ">
+                        <div class="w-[1rem] h-[1rem] flex items-center justify-center">
+                            <img src="../../../../public/icons/Estetica.svg" alt="">
+                        </div>
+                    </div>
+                    <h2 class=" ml-[1rem] text-gray-400 text-sm font-semibold">Estética</h2>
+                </li>
+
+            </ul>
     </aside>
 </body>
 </html>
+ 
