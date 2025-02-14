18676d8fb8b58a59c7ffa046603d2461493bd06e
gcosta <gcosta@grupoimagetech.com.br>
2025-02-14 07:56:23 -0400

    feat: alterações no componente

--- src/views/components/utils/inputComponent.php
+++ src/views/components/utils/inputComponent.php
@@ -2,23 +2,28 @@
  
     namespace Src\Views\Components\Utils;
 
-    function inputComponent(String $icon = "", String $type, String $label = "", String $placeholder, String $description = ""){
-        
-        $labelHtml = !empty($label) ? "<label class='text-sm font-medium text-gray-700'>$label</label>" : "";
-        
-        $descriptionHtml = !empty($description) ? "<p class='text-xs text-gray-500'>$description</p>" : "";
+    function inputComponent(string | null $icon, string $type, string | null $label, string $placeholder, string | null $description){
+        $icon = $icon ? "<img src='$icon'>" : "";
+        $label = $label ? "<label class='text-sm font-medium text-gray-700'>$label</label>" : "";
+        $description = $description ? "<p class='text-xs text-gray-500'>$description</p>" : "";
 
-        $iconHtml = !isset($icon) ? "<span class='absolute inset-y-0 left-3 flex items-center text-gray-400'>$icon</span>" : "";
+        // $labelHtml = !empty($label) ? "<label class='text-sm font-medium text-gray-700'>$label</label>" : "";
+        
+        // $descriptionHtml = !empty($description) ? "<p class='text-xs text-gray-500'>$description</p>" : "";
+
+        // $iconHtml = !isset($icon) ? "<span class='absolute inset-y-0 left-3 flex items-center text-gray-400'>
+        // <img src='$icon'>
+        // </span>" : "";
 
         return (
             "
             <div class='w-full'>
                 <div class='flex flex-col mb-1'> 
-                    $labelHtml
-                    $descriptionHtml
+                    $label
+                    $description
                 </div>
                 <div class='relative mt-1' > 
-                    $iconHtml
+                    $icon
                     <input type='$type' placeholder='$placeholder' class='w-full pr-3 py-2'>
                 </div>
             </div>
