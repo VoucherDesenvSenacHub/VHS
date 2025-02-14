16178762e6a9d226655f8b2b4701eb86ec4b17df
Guilherme Montealvão <guilhermehmc06@gmail.com>
2025-02-14 16:11:35 -0400

    feat: componente de input finalizado

--- src/views/components/utils/inputComponent.php
+++ src/views/components/utils/inputComponent.php
@@ -2,18 +2,22 @@
  
     namespace Src\Views\Components\Utils;
 
-    function inputComponent(string | null $icon, string $type, string | null $label, string $placeholder, string | null $description){
-        $icon = $icon ? "<img src='$icon'>" : "";
-        $label = $label ? "<label class='text-sm font-medium text-gray-700'>$label</label>" : "";
-        $description = $description ? "<p class='text-xs text-gray-500'>$description</p>" : "";
-
-        // $labelHtml = !empty($label) ? "<label class='text-sm font-medium text-gray-700'>$label</label>" : "";
+    function InputComponent(string $type, string $placeholder, string $icon = null, string $label = null, string $description = null, string $background = null, string $iconPosition = null){
         
-        // $descriptionHtml = !empty($description) ? "<p class='text-xs text-gray-500'>$description</p>" : "";
+        $icon = $icon ? "<img src='$icon' class='absolute $iconPosition w-4 h-4 fill-blue-500'>" : "";
+        
+        $margin = "";
 
-        // $iconHtml = !isset($icon) ? "<span class='absolute inset-y-0 left-3 flex items-center text-gray-400'>
-        // <img src='$icon'>
-        // </span>" : "";
+        $getPosition = explode("-",$iconPosition);
+        if($getPosition[0] == "left"){
+            $margin = "pl-10";
+        }
+
+        $label = $label ? "<label class='text-sm font-medium text-white mb-2'>$label</label>" : "";
+        
+        $description = $description ? "<p class='text-xs text-gray-200 mb-3'>$description</p>" : "";
+        
+        $background = $background ? "bg-$background" : "bg-transparent";
 
         return (
             "
@@ -22,9 +26,12 @@
                     $label
                     $description
                 </div>
-                <div class='relative mt-1' > 
-                    $icon
-                    <input type='$type' placeholder='$placeholder' class='w-full pr-3 py-2'>
+                <div class='relative mt-1 flex justify-center items-center'> 
+                    <input type='$type' placeholder='$placeholder' 
+                    class='w-full px-3 py-1.5 outline outline-1 outline-gray-500 rounded-md $background placeholder-slate-600 $margin text-zinc-200'
+                    onfocus='this.placeholder=\"\"' 
+                    onblur='this.placeholder=\"$placeholder\"
+                    '>$icon
                 </div>
             </div>
             "
