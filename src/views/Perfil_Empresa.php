037b650a5fb57fb6b0002500bb905403201f9873
João Felipe <jaofelipes22@gmail.com>
2025-02-17 17:14:11 -0400

    add

--- src/views/Perfil_Empresa.php
+++ src/views/Perfil_Empresa.php
@@ -8,23 +8,49 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Syne:wght@400..800&display=swap" rel="stylesheet">
 </head>
 <body class="w-full min-h-screen bg-gradient-to-b from-[#20002c] to-[#000000] bg-no-repeat bg-cover bg-center">
-    <div class="p-4 sm:p-6 md:p-8 mt-25 ml-25 sm:mt-50 sm:ml-50 ">
-        <div class="relative w-4/5 h-60 overflow-hidden z-0">
+    <div class="p-4 sm:p-6 md:p-8 mt-25 sm:mt-50 sm:ml-50 xs:p-4">
+        <div class="relative w-full h-36 xs:h-48 sm:h-60 overflow-hidden z-0">
             <!-- Imagem maior -->
             <img src="../../public/logos/B&W.jpg" class="absolute right-0 h-full w-full object-cover" alt="Grande">
         </div>
         
-        <div class="absolute left-70 top-95 w-40 h-40 rounded-[20%] border border-white/20 overflow-hidden z-10">
+        <div class="absolute xs:left-8 sm:left-70 top-55 xs:top-40 sm:top-95 w-24 xs:w-32 sm:w-40 h-24 xs:h-32 sm:h-40 rounded-[20%] border border-white/20 overflow-hidden z-10">
             <img src="../../public/logos/BillGates.webp" class="w-full h-full object-cover" alt="Pequena">
         </div>
-        <div>
-            <h1 class="text-white ml-55 bold text-[30px] font-[Poppins] mb-0">Microsoft</h1>
-            <div class="flex">
-            <p class="p-0 mt-0 text-gray-200 ml-55 bold text-[15px] font-[Poppins]">4.5k de seguidores</p>
-            <p class="p-0 mt-0 ml-1 text-gray-200 bold text-[15px] font-[Poppins]">|</p>
-            <p class="p-0 mt-0 ml-1 text-white bold text-[15px] font-[Poppins]">Parceiro</p>
-            <button class="w-[160px] bg-none text-white px-4 py-2 rounded-md border border-violet-500">Seguir</button>
+        <div class="mt-16 xs:mt-20 sm:mt-0">
+            <h1 class="text-white ml-2 xs:ml-4 sm:ml-55 text-xl xs:text-2xl sm:text-[30px] font-[Poppins] mb-0">Microsoft</h1>
+            <div class="flex flex-col sm:flex-row justify-between items-start sm:items-center">
+                <div class="flex flex-wrap ml-2 xs:ml-4 sm:ml-0">
+                    <p class="p-0 mt-0 text-gray-200 sm:ml-55 text-xs xs:text-sm sm:text-[15px] font-[Poppins]">4.5k de seguidores</p>
+                    <p class="p-0 mt-0 ml-1 text-gray-200 text-xs xs:text-sm sm:text-[15px] font-[Poppins]]">|</p>
+                    <p class="p-0 mt-0 ml-1 text-white text-xs xs:text-sm sm:text-[15px] font-[Poppins]">Parceiro</p>
+                </div>
+                <button class="w-[150px] xs:w-[150px] h-[35px] xs:h-[40px] mt-4 sm:-mt-6 bg-none text-white px-3 xs:px-4 py-1 xs:py-2 rounded-[10px] border border-violet-500 ml-2 xs:ml-4 text-sm xs:text-base">Seguir</button>
             </div>
+        </div>
+        <div>
+        <p class="text-white">Descriçao</p>
+        <div>
+
+            <table class="text-white">
+                <tr>
+                    <th>s</th>
+                    <th>s</th>
+                </tr>
+                <tr>
+                    <td><img src="" alt=""></td>
+                    <td><a href=""></a></td>
+                </tr>
+                <tr>
+                    <td><img src="" alt=""></td>
+                    <td><a href=""></a></td>
+                </tr>
+                <tr>
+                    <td><img src="" alt=""></td>
+                    <td><a href=""></a></td>
+                </tr>
+            </table>
+        </div>
         </div>
     </div>
     
