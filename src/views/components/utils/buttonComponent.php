3e42056bb94c3434373321629ca3556ad6f8f75a
Guilherme Montealvão <guilhermehmc06@gmail.com>
2025-02-16 20:11:44 -0400

    feat: novos parâmetros para a função

--- src/views/components/utils/buttonComponent.php
+++ src/views/components/utils/buttonComponent.php
@@ -1,13 +1,21 @@
 <?php
     
-    namespace Src\Components\Utils;
+    namespace Src\Views\Components\Utils;
 
-    function buttonComponent(bool | null $outline, string $text, string | null $icon, string $background) {
-        $outline = $outline ? "bg-none  outline-solid outline-purple-500" : '';
-        $icon = $icon ? "<img src='$icon' class='w-8 h-8'/>" : '';
+    function ButtonComponent(string $text, bool $outline = false, string $icon = null, string $background = null, string $outiline_color = null, string $text_color = null) {
+        
+        $outline = $outline ? "!bg-transparent outline outline-1 outline-$outiline_color" : '';
+        
+        $outiline_color = $outiline_color ? "outiline-$outiline_color" : "";
+        
+        $icon = $icon ? "<img src='$icon' class='absolute left-3 w-4 h-4 fill-blue-500'>" : "";
+
+        $text_color = $text_color ? "text-$text_color" : "";
+
+        $background = $background ? "!bg-$background" : "bg-white";
 
         echo "
-        <button class='flex justify-center items-center w-[380px] h-[50px] $outline $background'>
+        <button class='flex justify-center items-center w-[380px] h-[50px] relative gap-2 rounded-md cursor-pointer $outline $background $text_color'>
             $icon
             $text
         </button>
