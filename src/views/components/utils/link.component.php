cd72612c0d8148c8cc7bd6e7cf8188bb84ee9d49
Pedro Celestino <pedrojuniorcelestinocg@gmail.com>
2025-02-17 15:20:14 -0400

    ref: função

--- src/views/components/utils/link.component.php
+++ src/views/components/utils/link.component.php
@@ -11,17 +11,3 @@ function linkComponent($icon1,$link1,$text1){
 
 }
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../../../styles/global.css">
-    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
-    <title>Document</title>
-</head>
-<body class="bg-black">
-    <?= linkComponent("../../../../public/icons/lupa.svg","https://www.youtube.com/","youtube") ?>
-</body>
-</html>
