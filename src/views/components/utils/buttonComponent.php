afb103bc4eb8aeeab40d4b4f7ceb04311aa26f37
gcosta <gcosta@grupoimagetech.com.br>
2025-02-17 10:16:09 -0400

    pequenas mudanças

--- src/views/components/utils/buttonComponent.php
+++ src/views/components/utils/buttonComponent.php
@@ -2,20 +2,22 @@
     
     namespace Src\Views\Components\Utils;
 
-    function ButtonComponent(string $text, bool $outline = false, string $icon = null, string $background = null, string $outiline_color = null, string $text_color = null) {
+    function ButtonComponent(string $text, bool $outline = false, string $outiline_color = null, string $icon = null, string $background = null, string $text_color = null, string $font_size = null) {
         
-        $outline = $outline ? "!bg-transparent outline outline-1 outline-$outiline_color" : '';
+        $outline = $outline ? "!bg-transparent outline outline-1 $outiline_color" : '';
         
         $outiline_color = $outiline_color ? "outiline-$outiline_color" : "";
         
-        $icon = $icon ? "<img src='$icon' class='absolute left-3 w-4 h-4 fill-blue-500'>" : "";
-
-        $text_color = $text_color ? "text-$text_color" : "";
+        $icon = $icon ? "<img src='$icon' class='w-4 h-4'>" : "";
 
         $background = $background ? "!bg-$background" : "bg-white";
+        
+        $text_color = $text_color ? "text-$text_color" : "";
+
+        $font_size = $font_size ? "text-[$font_size]" : "";
 
         echo "
-        <button class='flex justify-center items-center w-[380px] h-[50px] relative gap-2 rounded-md cursor-pointer $outline $background $text_color'>
+        <button class='flex justify-center items-center w-[380px] h-[50px] gap-2 rounded-md cursor-pointer $outline $background $text_color $font_size'>
             $icon
             $text
         </button>
