0471bc00cc503a7161c47dc1f175e02b86944b38
Guilherme Montealvão <guilhermehmc06@gmail.com>
2025-02-17 14:54:15 -0400

    feat: componente de input de botao finalizado

--- src/views/components/utils/inputComponent.php
+++ src/views/components/utils/inputComponent.php
@@ -19,16 +19,9 @@
         
         $placeholder = htmlspecialchars($placeholder, ENT_QUOTES, 'UTF-8');
         
-        // $icon = $icon ? "<img src='$icon' class='absolute $iconPosition w-4 h-4 fill-blue-500'>" : "";
-
         $icon = $icon ? "<img src='" . htmlspecialchars($icon, ENT_QUOTES, 'UTF-8') . "' class='absolute $iconPosition w-4 h-4 fill-blue-500'>" : "";
 
         $margin = "";
-
-        // $getPosition = explode("-",$iconPosition);
-        // if($getPosition[0] == "left"){
-        //     $margin = "pl-10";
-        // }
 
         if (strpos($iconPosition, "left") !== false) {
             $margin = "pl-10";
@@ -37,24 +30,16 @@
             $margin = "pr-10";
         }
 
-        // $label = $label ? "<label class='$label_size font-medium text-white mb-2'>$label</label>" : "";
-
         $label = $label ? "<label class='$label_size font-medium text-white mb-2'>" . htmlspecialchars($label, ENT_QUOTES, 'UTF-8') . "</label>" : "";
 
         $label_size = $label_size ? "text-[$label_size]" : "text-sm";
-        
-        // $description = $description ? "<p class='$description_size text-gray-200 mb-3'>$description</p>" : "";
-
+    
         $description = $description ? "<p class='$description_size text-gray-200 mb-3'>" . htmlspecialchars($description, ENT_QUOTES, 'UTF-8') . "</p>" : "";
         
         $description_size = $description_size ? "text-[$description_size]" : "text-xs";
-        
-        // $background = $background ? "bg-$background" : "bg-transparent";
-
-        // $width = $width ? "w-[$width]" : "w-full";
 
         $classes = [
-            "px-3 py-1.5 outline outline-1 outline-gray-500 rounded-md placeholder-slate-600 text-zinc-200",
+            "!px-3 py-1.5 outline outline-1 outline-gray-500 rounded-md placeholder-slate-600 text-zinc-200 h-[35px]",
             $width = $width ? "w-[$width]" : "w-full",
             $background ? "bg-$background" : "bg-transparent",
             $margin
