18fcdcd6ee77141bbb3f0675da559a1014331447
Pedro Celestino <pedrojuniorcelestinocg@gmail.com>
2025-02-17 15:19:45 -0400

    ref: função

--- src/views/components/utils/link.component.php
+++ src/views/components/utils/link.component.php
@@ -1,19 +1,27 @@
 <?php
 namespace Src\Views\Components;
-function linkComponent($icon1,$icon2,$icon3,$link1,$link2,$link3,$text1,$text2,$text3){
+function linkComponent($icon1,$link1,$text1){
     return "
-    <div class='flex  gap-3.5 max-w-[21rem] max-h-[7.5rem]'>
+    <div class='flex gap-3.5 max-w-[21rem] max-h-[1.5rem]'>
         <div class='flex gap-6 size-[1.5rem] flex-col'>
             <img class='' src='$icon1'></img>
-            <img class='' src='$icon2'></img>
-            <img class='' src='$icon3'></img>
         </div>
-        <div class='flex gap-5.75 flex-col'>
         <a class='text-[#00DFFE]' target='_blank' href='$link1'>$text1</a>
-        <a class='text-[#00DFFE]' target='_blank' href='$link2'>$text2</a>
-        <a class='text-[#00DFFE]' target='_blank' href='$link3'>$text3</a>
-        </div>
     </div>";
 
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../../../styles/global.css">
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+    <title>Document</title>
+</head>
+<body class="bg-black">
+    <?= linkComponent("../../../../public/icons/lupa.svg","https://www.youtube.com/","youtube") ?>
+</body>
+</html>
