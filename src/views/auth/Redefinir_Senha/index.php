d51a9dd7f46a5cea076a75d422d9576213ba2059
HenriqueToyota <henriquetoyota13@gmail.com>
2025-02-19 16:13:51 -0400

    feat pagina redefinir senha

--- src/views/auth/Redefinir_Senha/index.php
+++ src/views/auth/Redefinir_Senha/index.php
@@ -1,52 +1,74 @@
 <?php
+namespace Src\Views\Components\Utils;
  
+require "../../components/utils/buttonComponent.php";
 require "../../components/utils/inputComponent.php";
            
 use function Src\Views\Components\Utils\InputComponent;
  
 ?>
+<title>Criar Conta</title>
+<script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+<style type="text/tailwindcss">
+  @theme {
+       
+    --text-pop: 'Poppins', sans-serif;
  
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="https://cdn.tailwindcss.com"></script>
-    <script>
-        tailwind.config = {
-            theme: {
-                extend: {
-                    colors: {
-                        background: '#0C0118',
-                        primary: "#6C00C0",
-                        secondary: "#fff",
-                        gray200: "#C4C4C4",
-                        gray300: "#666",
-                        gray600: "#1B1B1B"
-                    }
-                }
-            }
-        }  
-    </script>
-</head>
+    --primary: #6C00C0;
+    --secondary: #fff;
  
-<div class="flex w-100 h-screen bg-background  text-white " id="wrapper">
-        <div class="flex justify-center mr-24">
-            <img src="../../../../public/images/Cassete.svg" alt="" class=" relative right-[110px] mr-28 w-[1100px]">
+    --gray-200: #C4C4C4;
+    --gray-300: #666;
+    --gray-600: #1B1B1B;
+ 
+    --title-size: 2rem;
+    --subtitle-size: 1.5rem;
+    --paragraph-size: 1rem;
+  }
+ 
+      @layer utilities {
+    .font-pop {
+      font-family: var(--text-pop);
+    }
+ 
+    .title-size {
+      font-size: var(--title-size);
+    }
+ 
+    .subtitle-size {
+      font-size: var(--subtitle-size);
+    }
+ 
+    .paragraph-size {
+      font-size: var(--paragraph-size);
+    }
+  }
+</style>
+ 
+<div class="bg-[#0C0118]  h-full w-full items-center flex justify-between" >
+    <div class="flex items-center justify-center h-full ">
+        <img class="h-full relative right-[61px] mr-28 w-[1200px] " src="../../../../public/images/Cassete.svg" alt="">
+    </div>
+    <div class="mx-20  w-1/2 flex items-center justify-center shadow-[ -10px_0_30px_10px_rgba(255, 255, 255, 1)]">
+        <div class="flex flex-col gap-4">
+            <div class="flex items-center flex-col gap-4">
+                <img src="../../../../public/logos/Logo.svg" alt="">
+                <p class=" font-pop font-semibold title-size text-[#fff]">Redefinir Senha</p>
+                <p class=" font-pop paragraph-size text-gray-200">Informe o e-mail sua conta</p>
+            </div>
+            <div class="flex flex-col gap-4 w-[380px]">
+                <?= InputComponent(placeholder:"Insira seu e-mail", type:"email", label:"Email", icon:"../../../../public/icons/Vector.svg",iconPosition: "right-3") ?>
+                <div>
+                  <?= ButtonComponent("Continuar", "default") ?>
+                </div>
+                <div class="flex items-center text-white cursor-default">
+                  
+                </div>
+                </div>
+            </div>
         </div>
-           
-        <div class="flex jusfity-center mt-24  flex-col ">    
-            <div class="flex mt-20 ml-20 mb-5">
-                <img src="../../../../public/logos/Logo.svg" alt="Logo">
-            </div>
- 
-            <div class="">
-                <h2 class="flex ml-10 mt-0.7rem text-3xl font-semibold text-secondary">Redefinir Senha</h2>
-                <p class="text-gray200 flex  ml-10 mb-2 mt-2">Informe o e-mail de sua conta</p>
- 
-            </div>
- 
-            <div class="mb-5 w-[300px]">
-                <?= InputComponent(placeholder:"Insira seu e-mail", type:"email", label:"Email", icon:"../../../../public/images/E-mail.svg",iconPosition: "right-3") ?>
-            </div>
+    </div>
+</div>
+       
+<script>
+</script>
