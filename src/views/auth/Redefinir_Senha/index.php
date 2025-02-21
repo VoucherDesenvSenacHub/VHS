61b3a02a10a8705929b5c78f588b11bb22293f2f
HenriqueToyota <henriquetoyota13@gmail.com>
2025-02-21 17:06:11 -0400

    ref: correção das cores, usando a configuração do tailwind

--- src/views/auth/Redefinir_Senha/index.php
+++ src/views/auth/Redefinir_Senha/index.php
@@ -9,10 +9,10 @@ use function Src\Views\Components\Utils\InputComponent;
 ?>
 <title>Criar Conta</title>
 <script src="https://cdn.tailwindcss.com"></script>
-<script src="../../../styles/global.css"></script>
+<script src="../../../styles/tailwindglobal.js"></script>
 <body>
     
-<div class="bg-[#0C0118]  h-full w-full items-center flex justify-between" >
+<div class="bg-background  h-full w-full items-center flex justify-between" >
     <div class="flex items-center justify-center h-full ">
         <img class="h-full relative right-[61px] mr-28 w-[1200px] " src="../../../../public/images/Cassete.svg" alt="">
     </div>
@@ -20,8 +20,8 @@ use function Src\Views\Components\Utils\InputComponent;
         <div class="flex flex-col gap-4">
             <div class="flex items-center flex-col gap-4">
                 <img src="../../../../public/logos/Logo.svg" alt="">
-                <p class=" font-pop font-semibold title-size text-3xl text-[#fff]">Redefinir Senha</p>
-                <p class=" font-pop paragraph-size text-gray-300">Informe o e-mail sua conta</p>
+                <p class=" font-pop font-semibold title-size text-3xl text-secondary">Redefinir Senha</p>
+                <p class=" font-pop paragraph-size text-gray300">Informe o e-mail sua conta</p>
             </div>
             <div class="flex flex-col gap-4 w-[380px]">
                 <?= InputComponent(placeholder:"Insira seu e-mail", type:"email", label:"Email", icon:"../../../../public/icons/Vector.svg",iconPosition: "right-3") ?>
