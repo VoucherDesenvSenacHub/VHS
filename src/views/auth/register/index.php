f7fb0e3fd0bf6f74a0a0d34d2406d7aa5723e2c7
Bruna Gomes <brunagomes9165@gmail.com>
2025-02-24 16:21:56 -0400

    fix: Add function UserMenuComponent

--- src/views/auth/register/index.php
+++ src/views/auth/register/index.php
@@ -5,89 +5,57 @@ require "../../components/utils/buttonComponent.php";
 require "../../components/utils/inputComponent.php";
            
 use function Src\Views\Components\Utils\InputComponent;
-
 ?>
+
 <title>Criar Conta</title>
 <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
-<style type="text/tailwindcss">
-  @theme { 
-        
-    --text-pop: 'Poppins', sans-serif;
+<script src="../../../styles/tailwindglobal.js"></script>
 
-    --primary: #6C00C0;
-    --secondary: #fff;
-
-    --gray-200: #C4C4C4;
-    --gray-300: #666;
-    --gray-600: #1B1B1B;
-
-    --title-size: 2rem;
-    --subtitle-size: 1.5rem;
-    --paragraph-size: 1rem;
-  }
-
-      @layer utilities {
-    .font-pop {
-      font-family: var(--text-pop);
-    }
-
-    .title-size {
-      font-size: var(--title-size);
-    }
-
-    .subtitle-size {
-      font-size: var(--subtitle-size);
-    }
-
-    .paragraph-size {
-      font-size: var(--paragraph-size);
-    }
-  }
-</style>
-
-<div class="bg-[#0C0118]  h-full w-full items-center flex justify-between" >
-    <div class="flex items-center justify-center h-full ">
-        <img class="h-full relative right-[61px] mr-28 w-[1200px] " src="../../../../public/images/Cassete.svg" alt="">
-    </div>
-    <div class="mx-20  w-1/2 flex items-center justify-center shadow-[ -10px_0_30px_10px_rgba(255, 255, 255, 1)]">
-        <div class="flex flex-col gap-4">
-            <div class="flex items-center flex-col gap-2">
-                <img src="../../../../public/logos/Logo.svg" alt="">
-                <p class=" font-pop font-semibold title-size text-[#fff]">Criar sua Conta</p>
-                <p class=" font-pop paragraph-size text-gray-200">Informe seus dados para criar sua conta</p>
-            </div>
-            <div class="flex flex-col gap-4 w-96">
-                <?= InputComponent(placeholder:"Insira seu e-mail", type:"email", label:"Email", icon:"../../../../public/icons/Vector.svg",iconPosition: "right-3") ?>
-                <?= InputComponent(placeholder:"Insira sua senha", type:"password", label:"Senha", icon:"../../../../public/icons/eye-svgrepo.svg",iconPosition: "right-3") ?>
-                <?= InputComponent(placeholder:"Confirme sua senha", type:"password", label:"Confirmar senha", icon:"../../../../public/icons/eye-svgrepo.svg",iconPosition: "right-3") ?>
-                <div class="flex items-center space-x-2 cursor-pointer">
-                  <input type="checkbox" id="checkbox" class="hidden">
-                  <div id="customCheckbox" class="w-5 h-5 flex items-center justify-center bg-[#0C0118] rounded-md border border-[#666666] transition-all">
-                      <svg id="checkIcon" class="w-4 h-4 text-white hidden" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7"></path>
-                      </svg>
+<body>
+  <div class="bg-[#0C0118]  h-full w-full items-center flex justify-between" >
+      <div class="flex items-center justify-center h-full ">
+          <img class="h-full relative right-[61px] mr-28 w-[1200px] " src="../../../../public/images/Cassete.svg" alt="">
+      </div>
+      <div class="mx-20  w-1/2 flex items-center justify-center shadow-[ -10px_0_30px_10px_rgba(255, 255, 255, 1)]">
+          <div class="flex flex-col gap-4">
+              <div class="flex items-center flex-col gap-2">
+                  <img src="../../../../public/logos/Logo.svg" alt="">
+                  <p class=" font-pop font-semibold title-size text-[#fff]">Criar sua Conta</p>
+                  <p class=" font-pop paragraph-size text-gray-200">Informe seus dados para criar sua conta</p>
+              </div>
+              <div class="flex flex-col gap-4 w-96">
+                  <?= InputComponent(placeholder:"Insira seu e-mail", type:"email", label:"Email", icon:"../../../../public/icons/Vector.svg",iconPosition: "right-3") ?>
+                  <?= InputComponent(placeholder:"Insira sua senha", type:"password", label:"Senha", icon:"../../../../public/icons/eye-svgrepo.svg",iconPosition: "right-3") ?>
+                  <?= InputComponent(placeholder:"Confirme sua senha", type:"password", label:"Confirmar senha", icon:"../../../../public/icons/eye-svgrepo.svg",iconPosition: "right-3") ?>
+                  <div class="flex items-center space-x-2 cursor-pointer">
+                    <input type="checkbox" id="checkbox" class="hidden">
+                    <div id="customCheckbox" class="w-5 h-5 flex items-center justify-center bg-[#0C0118] rounded-md border border-[#666666] transition-all">
+                        <svg id="checkIcon" class="w-4 h-4 text-white hidden" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                          <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7"></path>
+                        </svg>
+                    </div>
+                    <span class="cursor-default text-white text-xs font-medium">Lembrar de mim</span>
                   </div>
-                  <span class="cursor-default text-white text-xs font-medium">Lembrar de mim</span>
-                </div>
-                <div>
-                  <?= ButtonComponent("Criar Conta", "default") ?>
-                </div>
-                <div class="flex items-center text-white cursor-default">
-                  <div class="flex-grow border-t border-[#666666]"></div>
-                  <span class="px-3 text-sm font-semibold">OU</span>
-                  <div class="flex-grow border-t border-[#666666]"></div>
-                </div>
-                <div>
-                  <?= ButtonComponent("Criar Conta pelo Google", "icon", "../../../../public/images/LogoGoogle.svg" ) ?>
-                </div>
-                <div class="flex gap-[2px] items-center justify-center">
-                  <p class="font-pop paragraph-size text-gray-200 cursor-default" >Já possui uma conta?</p>
-                  <a class="font-pop paragraph-size text-[#660BAD] underline" href="#">Entrar</a>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+                  <div>
+                    <?= ButtonComponent("Criar Conta", "default") ?>
+                  </div>
+                  <div class="flex items-center text-white cursor-default">
+                    <div class="flex-grow border-t border-[#666666]"></div>
+                    <span class="px-3 text-sm font-semibold">OU</span>
+                    <div class="flex-grow border-t border-[#666666]"></div>
+                  </div>
+                  <div>
+                    <?= ButtonComponent("Criar Conta pelo Google", "icon", "../../../../public/images/LogoGoogle.svg" ) ?>
+                  </div>
+                  <div class="flex gap-[2px] items-center justify-center">
+                    <p class="font-pop paragraph-size text-gray-200 cursor-default" >Já possui uma conta?</p>
+                    <a class="font-pop paragraph-size text-[#660BAD] underline" href="#">Entrar</a>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </div>
+</body>
         
 <script>
   const checkbox = document.getElementById("checkbox");
--- src/views/components/utils/userMenuComponent.php
+++ src/views/components/utils/userMenuComponent.php
@@ -1,89 +1,57 @@
-<script src="https://unpkg.com/@tailwindcss/browser@4"></script>
-<style type="text/tailwindcss">
-  @theme { 
-        
-    --text-pop: 'Poppins', sans-serif;
+<?php
+   namespace Src\Views\Components\Utils;
 
-    --primary: #6C00C0;
-    --secondary: #fff;
-
-    --gray-200: #C4C4C4;
-    --gray-300: #666;
-    --gray-600: #1B1B1B;
-
-    --title-size: 2rem;
-    --subtitle-size: 1.5rem;
-    --paragraph-size: 1rem;
-  }
-
-      @layer utilities {
-    .font-pop {
-      font-family: var(--text-pop);
-    }
-
-    .title-size {
-      font-size: var(--title-size);
-    }
-
-    .subtitle-size {
-      font-size: var(--subtitle-size);
-    }
-
-    .paragraph-size {
-      font-size: var(--paragraph-size);
-    }
-  }
-  
-</style>
-
-<div class="w-72 divide-y-1 divide-[#666666] h-auto bg-[#1b1b1b] rounded-xl">
-  <div class="flex items-center w-full gap-4 h-auto p-4">
-    <div class="rounded-full">
-      <img class="w-12 h-12 bg-red-500 rounded-full" src="../../../../public/images/ProfilePhoto.png" alt="">
-    </div>
-    <div class="h-14">
-      <p class="font-pop paragraph-size text-[#FFFFFF]">Freitasdev</p>
-      <p class="text-[#666]">eu@freitasdev...</p>
-      <p class="font-pop text-xs text-[#C4C4C4]">Desenvolvedor</p>
-    </div>
-  </div>
-  <div class="flex flex-col justify-center gap-4 p-4">
-    <div class="flex items-center gap-2">
-      <div>
-        <img class="w-6 h-6" src="../../../../public/icons/Settings.svg" alt="">
-      </div>
-      <div >
-        <p class="font-pop paragraph-size text-[#FFFFFF]">Minha Conta</p>
-        <p class="font-pop text-xs text-[#666]">Gerencie dados e preferência</p>
-      </div>
-    </div>
-    <div class="flex items-center gap-2">
-      <div>
-        <img class="w-6 h-6" src="../../../../public/icons/Studioo.svg" alt="">
-      </div>
-      <div >
-        <p class="font-pop paragraph-size text-[#FFFFFF]">VSH Studio</p>
-        <p class="font-pop text-xs text-[#666]">Gerencie seu canal</p>
-      </div>
-    </div>
-    <div class="flex items-center gap-2">
-      <div>
-        <img class="w-8 h-8" src="../../../../public/icons/Dashboardd.svg" alt="">
-      </div>
-      <div >
-        <p class="font-pop paragraph-size text-[#FFFFFF]">Dashboard</p>
-        <p class="font-pop text-xs text-[#666]">Gerencie os vídeos da plataforma, usuário e comentários</p>
-      </div>
-    </div>
-  </div>
-  <div class="flex items-center gap-2 p-4">
-    <div>
-      <img class="w-6 h-6" src="../../../../public/icons/exit.svg" alt="">
-    </div>
-    <div>
-      <p class="font-pop paragraph-size text-[#BC3636]">Sair da conta</p>
-    </div>
-  </div>
-</div>
-
-
+   function UserMenuComponent( string $nomeUsuario, string $emailUsuario, string $descricaoUsuario, string $fotoDePerfil){
+      return(
+        "<div class='w-72 divide-y-1 divide-[#666666] h-auto bg-[#1b1b1b] rounded-xl'>
+          <div class='flex items-center w-full gap-4 h-auto p-4'>
+            <div class='rounded-full'>
+              <img class='w-12 h-12 bg-red-500 rounded-full' src='$fotoDePerfil' alt='Foto de Perfil'>
+            </div>
+            <div class='h-14'>
+              <p class='font-pop paragraph-size text-[#FFFFFF]'>$nomeUsuario</p>
+              <p class='text-[#666] truncate w-40'>$emailUsuario</p>
+              <p class='font-pop text-xs text-[#C4C4C4]'>$descricaoUsuario</p>
+            </div>
+          </div>
+          <div class='flex flex-col justify-center gap-4 p-4'>
+            <div class='flex items-center gap-2'>
+              <div>
+                <img class='w-6 h-6' src='../../../../public/icons/Settings.svg' alt=''>
+              </div>
+              <div >
+                <p class='font-pop paragraph-size text-[#FFFFFF]'>Minha Conta</p>
+                <p class='font-pop text-xs text-[#666]'>Gerencie dados e preferência</p>
+              </div>
+            </div>
+            <div class='flex items-center gap-2'>
+              <div>
+                <img class='w-6 h-6' src='../../../../public/icons/Studioo.svg' alt=''>
+              </div>
+              <div >
+                <p class='font-pop paragraph-size text-[#FFFFFF]'>VSH Studio</p>
+                <p class='font-pop text-xs text-[#666]'>Gerencie seu canal</p>
+              </div>
+            </div>
+            <div class='flex items-center gap-2'>
+              <div>
+                <img class='w-8 h-8' src='../../../../public/icons/Dashboardd.svg' alt=''>
+              </div>
+              <div >
+                <p class='font-pop paragraph-size text-[#FFFFFF]'>Dashboard</p>
+                <p class='font-pop text-xs text-[#666]'>Gerencie os vídeos da plataforma, usuário e comentários</p>
+              </div>
+            </div>
+          </div>
+          <div class='flex items-center gap-2 p-4'>
+            <div>
+              <img class='w-6 h-6' src='../../../../public/icons/exit.svg' alt=''>
+            </div>
+            <div>
+              <p class='font-pop paragraph-size text-[#BC3636]'>Sair da conta</p>
+            </div>
+          </div>
+        </div>"
+      );
+   }
+?>
