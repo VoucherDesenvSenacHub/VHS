1864153fe2cafd7457c73df907f6aa4b8aa4f057
HenriqueToyota <henriquetoyota13@gmail.com>
2025-02-24 14:56:33 -0400

    ref: ♻️ ajustes nas classes do código

--- src/views/auth/Redefinir_Senha/index.php
+++ src/views/auth/Redefinir_Senha/index.php
@@ -6,6 +6,7 @@ require "../../components/utils/buttonComponent.php";
 require "../../components/utils/inputComponent.php";
 
 use function Src\Views\Components\Utils\InputComponent;
+use function Src\Views\Components\Utils\ButtonComponent;
 
 ?>
 
@@ -19,15 +20,14 @@ use function Src\Views\Components\Utils\InputComponent;
     <script src="../../../styles/tailwindglobal.js"></script>
 </head>
 
-<body>
-
-    <div class="bg-background  h-full w-full items-center flex justify-between">
-        <div class="flex items-center justify-center h-full ">
-            <img class="h-full relative right-[61px] mr-28 w-[1200px] " src="../../../../public/images/Cassete.svg" alt="">
+<body class="bg-background h-screen w-screen flex justify-between items-center">
+    <div class="flex">
+        <div class="flex justify-center mr-24">
+            <img src="../../../../public/images/Cassete.svg" alt="" class="relative right-[110px] mr-28 w-[1100px]">
         </div>
-        <div class="mx-20  w-1/2 flex items-center justify-center shadow-[ -10px_0_30px_10px_rgba(255, 255, 255, 1)]">
-            <div class="flex flex-col gap-4">
-                <div class="flex items-center flex-col gap-4">
+        <div class="flex items-center justify-center">
+            <div class="flex flex-col gap-2">
+                <div class="flex items-center flex-col gap-2">
                     <img src="../../../../public/logos/Logo.svg" alt="">
                     <p class=" font-pop font-semibold title-size text-3xl text-secondary">Redefinir Senha</p>
                     <p class=" font-pop paragraph-size text-gray300">Informe o e-mail sua conta</p>
@@ -35,17 +35,12 @@ use function Src\Views\Components\Utils\InputComponent;
                 <div class="flex flex-col gap-4 w-[380px]">
                     <?= InputComponent(placeholder: "Insira seu e-mail", type: "email", label: "Email", icon: "../../../../public/icons/Vector.svg", iconPosition: "right-3") ?>
                     <div>
-                        <?= ButtonComponent("Continuar", "default") ?>
-                    </div>
-                    <div class="flex items-center text-white cursor-default">
-
+                        <?= ButtonComponent(text: "Enviar e-mail", variant: "default") ?>
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    </div>
-
 </body>
 
 </html>
