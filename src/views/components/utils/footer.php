d198eb41703bd826c687ba01d60744c299bde8f7
João Augusto Cintra Campos <joaoaugustocintracampos@gmail.com>
2025-03-07 15:20:57 -0400

    update footer php

--- src/views/components/utils/footer.php
+++ src/views/components/utils/footer.php
@@ -1,9 +1,9 @@
 <?php
     function footer(){
         echo "
-            <div class='flex w-[90rem] h-[12.0625rem]'>
-        <div class='w-[11.5625rem] h-[3.0625rem] ml-[4.375rem] mt-[6.5rem] '><img src='../../../../public/images/imagefooter_1.png' alt='imagem_1'></div>
-        <div class='w-[7.125rem] h-[6.421rem] ml-[65.3125rem] mt-[4.8125rem] '><img src='../../../../public/images/imagefooter_2.png' alt='imagem_2'></div>
+       <div class='flex w-[90rem] h-[12.0625rem] justify-between'>
+        <div class='w-[11.5625rem] h-[3.0625rem] ml-[4.375rem] mt-[6.5rem] '><img src='imagefooter_1.png' alt='imagem_1'></div>
+        <div class='w-[7.125rem] h-[6.421rem] mr-[1.62rem] mt-[4.8125rem] '><img src='imagefooter_2.png' alt='imagem_2'></div>
     </div>
         
         ";
