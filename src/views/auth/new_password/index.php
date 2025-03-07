263b757124c0d3b160b9ee18627af74f76c42b99
LuanEnrico30 <luanzinhoenrico@gmail.com>
2025-03-07 16:32:03 -0400

    ref: igualei página de login, página de register e página de new_password

---
+++ src/views/auth/new_password/index.php
@@ -0,0 +1,49 @@
+<?php
+
+require "../../components/utils/inputComponent.php";
+require "../../components/utils/buttonComponent.php";
+
+use function Src\Views\Components\Utils\InputComponent;
+use function Src\Views\Components\Utils\ButtonComponent;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>new_password</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="../../../styles/tailwindglobal.js"></script>
+</head>
+
+<body>
+    <div class="flex w-100 h-screen bg-background text-white">
+        <div class="flex justify-center mr-24">
+            <img src="../../../../public/images/Cassete.svg" alt="" class="relative right-[61px] mr-28 w-[1392px]">
+        </div>
+
+        <div class="relative mx-20 w-1/2 flex items-center justify-center shadow-[-10px_0_30px_10px_rgba(255, 255, 255, 1)]">
+            <div class="flex flex-col gap-4">
+                <div class="flex items-center flex-col gap-2">
+                    <img src="../../../../public/logos/Logo.svg" alt="">
+                    <p class="text-[33px] font-pop font-semibold title-size text-[#fff]">Redefenir senha</p>
+                    <p class="font-pop paragraph-size text-gray-200">Insira uma nova senha</p>
+                </div>
+                <div class="flex flex-col gap-4 w-96">
+                    <?= InputComponent(placeholder: "Insira sua senha nova", type: "password", label: "Senha", icon: "../../../../public/icons/eye-svgrepo.svg", iconPosition: "w-6 h-6 right-3") ?>
+                    <?= InputComponent(placeholder: "Confirme nova senha", type: "password", label: "Confirme sua senha", icon: "../../../../public/icons/eye-svgrepo.svg", iconPosition: "w-6 h-6 right-3") ?>
+                    <div class="mt-2">
+                        <?= ButtonComponent("Acessar plataforma", "default") ?>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    </script>
+</body>
+
+</html>
