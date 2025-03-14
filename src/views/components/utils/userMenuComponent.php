da7748c238d3fe55cb4c6c6a443c078db0a6fd87
Bruna Gomes <brunagomes9165@gmail.com>
2025-03-14 15:27:02 -0400

    fix: arrumando

--- src/views/components/utils/userMenuComponent.php
+++ src/views/components/utils/userMenuComponent.php
@@ -3,15 +3,15 @@
 
    function UserMenuComponent( string $nomeUsuario, string $emailUsuario, string $descricaoUsuario, string $fotoDePerfil){
       return(
-        "<div class='w-72 divide-y-1 divide-[#666666] h-auto bg-[#1b1b1b] rounded-xl'>
+        "<div class='w-72 divide-y-[1px] divide-gray300 h-auto bg-gray600 rounded-xl'>
           <div class='flex items-center w-full gap-4 h-auto p-4'>
             <div class='rounded-full'>
               <img class='w-12 h-12 bg-red-500 rounded-full' src='$fotoDePerfil' alt='Foto de Perfil'>
             </div>
             <div class='h-14'>
-              <p class='font-pop paragraph-size text-[#FFFFFF]'>$nomeUsuario</p>
-              <p class='text-[#666] truncate w-40'>$emailUsuario</p>
-              <p class='font-pop text-xs text-[#C4C4C4]'>$descricaoUsuario</p>
+              <p class='font-pop paragraph-size text-secondary'>$nomeUsuario</p>
+              <p class='text-gray300 truncate w-40'>$emailUsuario</p>
+              <p class='font-pop text-xs text-gray200'>$descricaoUsuario</p>
             </div>
           </div>
           <div class='flex flex-col justify-center gap-4 p-4'>
@@ -20,8 +20,8 @@
                 <img class='w-6 h-6' src='../../../../public/icons/Settings.svg' alt=''>
               </div>
               <div >
-                <p class='font-pop paragraph-size text-[#FFFFFF]'>Minha Conta</p>
-                <p class='font-pop text-xs text-[#666]'>Gerencie dados e preferência</p>
+                <p class='font-pop paragraph-size text-secondary hover:text-primary'>Minha Conta</p>
+                <p class='font-pop text-xs text-gray300'>Gerencie dados e preferência</p>
               </div>
             </div>
             <div class='flex items-center gap-2'>
@@ -29,17 +29,17 @@
                 <img class='w-6 h-6' src='../../../../public/icons/Studioo.svg' alt=''>
               </div>
               <div >
-                <p class='font-pop paragraph-size text-[#FFFFFF]'>VSH Studio</p>
-                <p class='font-pop text-xs text-[#666]'>Gerencie seu canal</p>
+                <p class='font-pop paragraph-size text-secondary hover:text-primary'>VSH Studio</p>
+                <p class='font-pop text-xs text-gray300'>Gerencie seu canal</p>
               </div>
             </div>
             <div class='flex items-center gap-2'>
-              <div>
+              <div class='hover:stroke-cyan-500'>
                 <img class='w-8 h-8' src='../../../../public/icons/Dashboardd.svg' alt=''>
               </div>
               <div >
-                <p class='font-pop paragraph-size text-[#FFFFFF]'>Dashboard</p>
-                <p class='font-pop text-xs text-[#666]'>Gerencie os vídeos da plataforma, usuário e comentários</p>
+                <p class='font-pop paragraph-size text-secondary hover:text-primary'>Dashboard</p>
+                <p class='font-pop text-xs text-gray300'>Gerencie os vídeos da plataforma, usuário e comentários</p>
               </div>
             </div>
           </div>
@@ -48,7 +48,7 @@
               <img class='w-6 h-6' src='../../../../public/icons/exit.svg' alt=''>
             </div>
             <div>
-              <p class='font-pop paragraph-size text-[#BC3636]'>Sair da conta</p>
+              <p class='font-pop paragraph-size text-red600'>Sair da conta</p>
             </div>
           </div>
         </div>"
