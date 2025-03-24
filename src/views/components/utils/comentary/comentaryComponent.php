3b2bbf0ea541542ec1b8e9e97af3f117a968d09d
João Augusto Cintra Campos <joaoaugustocintracampos@gmail.com>
2025-03-24 16:29:13 -0400

    Update-3

--- src/views/components/utils/comentary/comentaryComponent.php
+++ src/views/components/utils/comentary/comentaryComponent.php
@@ -3,16 +3,16 @@
 function comentary(){
 
     echo '
-    <div class="max-w-96 h-28 block flex">
+  <div class="max-w-96 h-28 block flex">
 
-        <div class="w-10 h-11 rounded-[100%] mt-mt-1">
+        <div class="w-10 h-11 rounded-full mt-mt-1">
             <img class="w-full h-full rounded-full" src="../../../../../public/images/image.svg" alt="">
         </div>
 
 
         <div class="block ml-3">
 
-            <div class="max-w-80 h-5 flex items-baseline"><p class="text-sm text-white font-semibold">João Silva</p> <p class="text-xs text-gray-300 font-semibold ml-[1]">há 5 dias</p></div>
+            <div class="max-w-80 h-5 flex items-baseline"><p class="text-sm text-white font-semibold">João Silva</p> <p class="text-xs text-gray-300 font-semibold ml-1">há 5 dias</p></div>
 
             <div class="max-w-xs h-20 mt-1 text-xs font-semibold text-gray-400">Pellentesque viverra pulvinar justo nec sagittis. Nunc nec mi et dui convallis consectetur pharetra at lectus. Nunc porta urna a diam bibendum, rhoncus sollicitudin justo lacinia. Vivamus faucibus enim augue.</div>
 
@@ -22,18 +22,16 @@ function comentary(){
             <img src="3botao.svg" alt="3_botao">
         <div>
 </div>
-
-
 <div class="w-16 h-14 bg-gray-800 rounded-[0.22rem] flex items-center justify-center  border-[0.1rem] border-solid border-gray-600  top-[-0.56rem] ml-1 absolute  hidden" id="menu">
 
-    <ul class="w-[100%]">  
+    <ul class="w-full">  
         <li class=" hover:bg-gray-700 text-white font-semibold flex justify-center items-center  w-14 h-5 text-xs m-auto">
 
             <div>
                 <img src="trash.svg" alt=
                 "">
             </div>
-            <div class="ml-[1]">
+            <div class="ml-1">
                 <p>Excluir</p>
             </div>
         </li>
@@ -41,13 +39,14 @@ function comentary(){
             <div>
                 <img src="pen.svg" alt="">
             </div>
-            <div class="ml-[1]">
+            <div class="ml-1">
                 <p>Editar</p>
             </div>
         </li>
         
     </ul>    
 </div>
+
 
 ';
 
