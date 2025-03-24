d809dd756c20b50a10bc4dcabff857e4159aa6f0
Ronan <ronanscampos@hotmail.com>
2025-03-24 16:05:33 -0400

    UPDATE: Criando pasta para o component header e mudando o nome do script e modificando rotas dos icones

--- src/views/components/header/headerComponent.php
+++ src/views/components/header/headerComponent.php
@@ -7,9 +7,9 @@
         <header id="header" class="flex items-center justify-between p-5 shadow-md">
         <div class="flex items-center gap-4">
           <button id="menu" class="p-2 rounded-lg">
-              <img src="../../../../public/header/Menu.svg" alt="">
+              <img src="../../../public/icons/header/Menu.svg" alt="">
           </button>
-          <img src="../../../../public/logos/Logo.svg" alt="Logo" class="h-8">
+          <img src="../../../public/logos/Logo.svg" alt="Logo" class="h-8">
         </div>
       
         <div class="flex items-center gap-4">
@@ -20,6 +20,6 @@
           <img src="../../../public/images/Avatar.svg" alt="Foto de perfil" class="h-8 w-8 rounded-full p-41">
         </div>
       </header>
-      <script src="./script.js"></script>
+      <script src="./header/headerScript.js"></script>
         ';
     }
