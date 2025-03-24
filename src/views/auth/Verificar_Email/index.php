728eef2e5b84d39e702f472761a9424835d4c4a7
HenriqueToyota <henriquetoyota13@gmail.com>
2025-03-24 15:22:09 -0400

    ref: correção dos tamanhos e div adicionada

--- src/views/auth/Verificar_Email/index.php
+++ src/views/auth/Verificar_Email/index.php
@@ -2,7 +2,7 @@
 
 require "../../components/utils/inputComponent.php";
 require "../../components/utils/buttonComponent.php";
-            
+
 use function Src\Views\Components\Utils\InputComponent;
 use function Src\Views\Components\Utils\ButtonComponent;
 
@@ -10,6 +10,7 @@ use function Src\Views\Components\Utils\ButtonComponent;
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -19,24 +20,26 @@ use function Src\Views\Components\Utils\ButtonComponent;
 </head>
 
 <body>
-    <div class="flex w-100 h-screen bg-background text-white">
+    <div class="flex h-screen bg-background text-white">
         <div class="flex justify-center mr-24">
-            <img src="../../../../public/images/Cassete.svg" alt="" class="relative right-[110px] mr-28 w-[1100px]">
+            <img src="../../../../public/images/Cassete.svg" alt="" class="relative right-14 mr-28 w-6/16">
         </div>
 
-        <div class="gap-4 flex mt-24 flex-col">    
-            <div class="flex mt-20 ml-24 mb-5 justify-center ml-8">
-                <img src="../../../../public/logos/Logo.svg" alt="Logo">
-            </div>
-            <div>
-                <h2 class="flex ml-10  text-3xl font-semibold text-secondary justify-center mr-2">Quase Lá</h2>
-                <p class="text-gray200 flex ml-3 mb-2 mt-2 justify-center ml-5">Por favor verifique sua caixa de e-mail</p>
-            </div>
-            <div>
-                <img src="../../../../public/images/catGif.gif" alt="" class="flex rounded-lg w-80 justify-center ml-5">
-            </div>
-            <div class="justify-center ml-5">
-                <?= ButtonComponent(text: "Já verifiquei", variant: "default") ?>
+        <div class="relative mx-20 w-1/2 flex items-center justify-center shadow-[-10px_0_30px_10px_rgba(255, 255, 255, 1)]">
+            <div class="flex flex-col gap-4">
+                <div class="flex items-center flex-col gap-2">
+                    <img src="../../../../public/logos/Logo.svg" alt="">
+                    <p class="text-4xl font-pop font-semibold title-size text-white">Quase lá</p>
+                    <p class="font-pop paragraph-size text-gray-200">Por favor verifique sua caixa de e-mail</p>
+                </div>
+
+                <div>
+                    <img src="../../../../public/images/catGif.gif" alt="" class="flex rounded-lg w-80 justify-center ml-5">
+                </div>
+                
+                <div class="justify-center ml-5">
+                    <?= ButtonComponent(text: "Já verifiquei", variant: "default") ?>
+                </div>
             </div>
         </div>
     </div>
