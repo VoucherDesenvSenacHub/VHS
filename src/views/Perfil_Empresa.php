76a6e17fb40bc213fc45d87d255960714dde83da
João Felipe <jaofelipes22@gmail.com>
2025-03-24 15:07:46 -0400

    💄 - update bg color

--- src/views/Perfil_Empresa.php
+++ src/views/Perfil_Empresa.php
@@ -23,7 +23,7 @@ use function src\views\components\utils\footerComponent;
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Syne:wght@400..800&display=swap" rel="stylesheet">
 </head>
-<body class="w-full min-h-screen bg-gradient-to-b from-[#20002c] to-[#000000] bg-no-repeat bg-cover bg-center">
+<body class="w-full min-h-screen bg-gradient-to-b from-[#20002c] via-black to-[#20002c] bg-no-repeat bg-cover bg-center">
     <div>
         <?php echo HeaderComponent(); ?>
     </div>
