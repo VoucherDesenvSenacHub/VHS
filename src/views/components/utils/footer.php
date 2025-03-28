e380c611f3a405b675ea0e9099881424883396fa
João Augusto Cintra Campos <joaoaugustocintracampos@gmail.com>
2025-03-28 14:54:15 -0400

    update footer 28-03-25

--- src/views/components/utils/footer.php
+++ src/views/components/utils/footer.php
@@ -1,12 +1,12 @@
 <?php
     function footer(){
-        echo '
-       <div class="flex w-full h-48 justify-between  items-center p-16 "> 
-        <div class="w-44 h-13 ml-16 "><img src="imagefooter_1.png" alt="imagem_1"></div>
-        <div class="w-28 h-28 mr-7 "><img src="imagefooter_2.png" alt="imagem_2"></div>
+        return "
+       <div class='flex w-full h-48 justify-between  items-center p-16 '> 
+        <div class='w-44 h-13 ml-16 ''><img src='../../../images/imagefooter_1.png imagefooter_1.png' alt='imagem_1'></div>
+        <div class='w-28 h-28 mr-7 '><img src='../../../images/imagefooter_2.png' alt='imagem_2'></div>
     </div>
         
-        ';
+        ";
     }
 ?>
 
