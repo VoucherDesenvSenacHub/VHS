330bb0f451d43e1a961e0d8a2fd4ad1105caf269
João Augusto Cintra Campos <joaoaugustocintracampos@gmail.com>
2025-03-28 15:08:14 -0400

    ref; mudança na altura

--- src/views/components/utils/footer.php
+++ src/views/components/utils/footer.php
@@ -3,7 +3,7 @@
         return (
             "
                 <div class='flex w-full h-48 justify-between items-center p-16'> 
-                    <div class='w-44 h-13 ml-16'>
+                    <div class='w-44 h-12 ml-16'>
                         <img src='./Voucher-dev-142-VHS/public/images/imagefooter_1.png' alt='imagem_1'>
                     </div>
                     <div class='w-28 h-28 mr-7'>
