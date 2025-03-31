936181496126335cab86e3f5116497cf9bffae64
Bruna Gomes <brunagomes9165@gmail.com>
2025-03-31 14:52:28 -0400

    feat: tela de register2

--- src/views/auth/register/index.php
+++ src/views/auth/register/index.php
@@ -20,13 +20,13 @@ use function Src\Views\Components\Utils\InputComponent;
           <div class="flex flex-col gap-4">
               <div class="flex items-center flex-col gap-2">
                   <img src="../../../../public/logos/Logo.svg" alt="">
-                  <p class=" font-pop font-semibold title-size text-secondary">Criar sua Conta</p>
-                  <p class=" font-pop paragraph-size text-gray-200">Informe seus dados para criar sua conta</p>
+                  <p class=" font-pop font-semibold text-3xl text-secondary">Criar sua Conta</p>
+                  <p class=" font-pop paragraph-size text-gray-200">Informe seus dados para criar sua conta.</p>
               </div>
               <div class="flex flex-col gap-4 w-96">
-                  <?= InputComponent(placeholder:"Insira seu e-mail", type:"email", label:"Email", icon:"../../../../public/icons/Vector.svg",iconPosition: "right-3") ?>
-                  <?= InputComponent(placeholder:"Insira sua senha", type:"password", label:"Senha", icon:"../../../../public/icons/eye-svgrepo.svg",iconPosition: "right-3") ?>
-                  <?= InputComponent(placeholder:"Confirme sua senha", type:"password", label:"Confirmar senha", icon:"../../../../public/icons/eye-svgrepo.svg",iconPosition: "right-3") ?>
+                  <?= InputComponent(placeholder:"Insira seu Usuário", type:"text", label:"Usuário", icon:"../../../../public/icons/userRound.svg",iconPosition: "right-3") ?>
+                  <?= InputComponent(placeholder:"Insira seu E-mail", type:"email", label:"Email", icon:"../../../../public/icons/mail.svg",iconPosition: "right-3") ?>
+                  <?= InputComponent(placeholder:"Insira sua senha" ,type:"date", label:"Data de Nascimento", icon:"../../../../public/icons/calendarDays.svg",iconPosition: "right-3") ?>
                   <div class="flex items-center space-x-2 cursor-pointer">
                     <input type="checkbox" id="checkbox" class="hidden">
                     <div id="customCheckbox" class="w-5 h-5 flex items-center justify-center bg-background rounded-md border border-gray600 transition-all">
@@ -45,11 +45,11 @@ use function Src\Views\Components\Utils\InputComponent;
                     <div class="flex-grow border-t border-gray300"></div>
                   </div>
                   <div>
-                    <?= ButtonComponent("Criar Conta pelo Google", "icon", "../../../../public/images/LogoGoogle.svg" ) ?>
+                    <?= ButtonComponent("Criar Conta pelo Google", "icon", "../../../../public/images/LogoGoogle.svg") ?>
                   </div>
                   <div class="flex gap-[2px] items-center justify-center">
                     <p class="font-pop paragraph-size text-secondary cursor-default" >Já possui uma conta?</p>
-                    <a class="font-pop paragraph-size text-primary underline" href="#">Entrar</a>
+                    <a class="font-pop paragraph-size text-primary underline" href="../login/index.php">Entrar</a>
                   </div>
               </div>
           </div>
