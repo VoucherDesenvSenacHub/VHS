88d4d9693d200d8b2f56a6caac5be3c7e280bad1
Guilherme Mendes <guilherme54098206@aluno.ms.senac.br>
2025-04-14 15:10:03 -0400

    Removendo Arquivos

--- src/vendor/routes.autoload.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../controllers/signUp.controller.php';
-require_once __DIR__ . '/../controllers/verifyEmail.controller.php';
--- src/views/components/utils/comentary/script.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.getElementById('opcoes');
-const menu = document.getElementById('menu');
-
-menuBtn.addEventListener('click', () => {
-    menu.style.display = menu.style.display === 'flex' ? 'none' : 'flex';
-});
