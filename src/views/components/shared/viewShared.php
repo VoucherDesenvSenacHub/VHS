c5880268db732b6dd5d53d31763c8afb792e1b15
Josué <josuealexandre067@gmail.com>
2025-05-05 15:20:10 -0400

    1

---
+++ src/views/components/shared/viewShared.php
@@ -0,0 +1,27 @@
+<?php
+
+    require_once './shared.php';
+
+?>
+
+<!-- H T M L -->
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Visualização</title>
+
+    <link rel="stylesheet" href="../../../styles/global.css">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body class="w-[100vw] h-[100vh] flex items-center justify-center bg-gray-900">
+<div id="container" class="w-full h-full flex items-center justify-center">
+    
+    <button name="send" class="border-0 bg-blue-600 p-4 rounded-full font-bold text-white shadow-md hover:bg-blue-500">Compartilhar</button>
+
+</div>
+</body>
+</html>
