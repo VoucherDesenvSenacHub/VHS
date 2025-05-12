96f6b5fe979d5de9ae59508a916a82383b5c7815
Kayke Adriano <kayke.adrianobdg@gmail.com>
2025-05-12 16:23:38 -0400

    creat do icons

--- src/views/components/shared/shared.php
+++ src/views/components/shared/shared.php
@@ -5,5 +5,10 @@
             <div></div>
         `;
     }
-
 ?>
+
+<img src="../../../../public/icons/telegram.svg" alt="">
+<img src="../../../../public/icons/facebook.svg" alt="">
+<img src="../../../../public/icons/whats.svg" alt="">
+<img src="../../../../public/icons/insta.svg" alt="">
+<img src="../../../../public/icons/twitter.svg" alt="">
