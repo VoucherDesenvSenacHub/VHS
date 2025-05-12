943741f62aee09ea7d0f38f2e2b6277f140053a2
Samuel de Barros Sousa <mkcsamu@gmail.com>
2025-05-12 17:05:21 -0300

    ref-CadUltimoVideo
    arrumei os cainhos e estrutura do codigo

--- src/views/components/CardDUltimoVideo/card.php
+++ src/views/components/CardDUltimoVideo/card.php
@@ -3,13 +3,13 @@
 namespace src\views\components\header;
 require "./UltimoVideo.php";
 function Card(){ 
-    echo " 
+    return " 
     <div class='flex bg-[#1B1B1B] w-[50.75rem] h-[19.81rem] rounded-[1rem] border-1 border-[#494949] border-solid'>
     <div class='flex flex-col relative top-[0.8rem] relative left-[2rem] gap-[1.5rem]'>
-        <div><p class='text-white relative bottom-[0.12rem] text-[1.2rem]'>Ultimos vídeos</p></div> " . 
-        VideoCard('https://www.animeunited.com.br/oomtumtu/2021/06/photo5064584801213851896.jpg', 'chup chupshuerps', 892393)
-    .   
-    "</div>
+        <div>
+            <p class='text-white relative bottom-[0.12rem] text-[1.2rem]'>Ultimos vídeos</p>
+        </div> 
+    </div>
     </div>
     ";
 }
