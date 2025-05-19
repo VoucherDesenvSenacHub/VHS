64612aee023dd2f81c2dc56717f3e87913915605
____x_ <josuealexandre067@gmail.com>
2025-05-18 23:48:28 -0400

    .

--- src/views/components/shared/shared.php
+++ src/views/components/shared/shared.php
@@ -1,14 +1,45 @@
 <?php
 
     function sharedComponent() {
-        echo `
-            <div></div>
-        `;
+        echo '
+            <div class="hidden modal absolute flex inset-0 bg-black/25 items-center justify-center">
+                <div class="flex flex-col bg-[#1B1B1B] w-auto h-64 rounded-2xl p-6 justify-between shadow-xl">
+                    <div class="w-full flex flex-row justify-between items-center">
+                        <h2 class="text-white text-xl">Compartilhar</h2>
+            
+                        <button onclick="closePopup()" class="flex justify-center items-center w-7 text-purple-400 hover:text-purple-600">
+                            <img src="../../../../public/icons/botaoFechar.svg" class="w-full h-full">
+                        </button>
+                    </div>
+
+                    <div class="flex justify-between">
+                        <button id="telegram" class="flex items-center justify-center w-12 h-12 bg-white/5 rounded-xl overflow-hidden">
+                            <img src="../../../../public/icons/telegram.svg" class="w-full h-full">
+                        </button>
+
+                        <button id="facebook" class="flex items-center justify-center w-12 h-12 bg-white/5 rounded-xl overflow-hidden">
+                            <img src="../../../../public/icons/facebook.svg" class="w-full h-full">
+                        </button>
+
+                        <button id="whatsapp" class="flex items-center justify-center w-12 h-12 bg-white/5 rounded-xl overflow-hidden">
+                            <img src="../../../../public/icons/whats.svg" class="w-full h-full">
+                        </button>
+
+                        <button id="instagram" class="flex items-center justify-center w-12 h-12 bg-white/5 rounded-xl overflow-hidden">
+                            <img src="../../../../public/icons/insta.svg" class="w-full h-full">
+                        </button>
+                        
+                        <button id="twitter" class="flex items-center justify-center w-12 h-12 bg-white/5 rounded-xl overflow-hidden">
+                            <img src="../../../../public/icons/twitter.svg" class="w-full h-full">
+                        </button>
+                    </div>
+
+                    <div class="flex flex-row justify-between gap-3">
+                        <input type="text" value="https://youtube.com/@freitasdev" class="focus:outline-none text-gray-500 text-sm p-2 w-72 border-[1px] border-[#666666] bg-transparent rounded-[10px]">
+                        <button class="bg-[#6C00C0] p-3 px-7 rounded-[10px] text-white">Copiar</button>
+                    </div>
+                </div>
+            </div>
+        ';
     }
 ?>
-
-<!-- <img src="../../../../public/icons/telegram.svg" alt="">
-<img src="../../../../public/icons/facebook.svg" alt="">
-<img src="../../../../public/icons/whats.svg" alt="">
-<img src="../../../../public/icons/insta.svg" alt="">
-<img src="../../../../public/icons/twitter.svg" alt=""> -->
--- src/views/components/shared/viewShared.php
+++ src/views/components/shared/viewShared.php
@@ -21,58 +21,16 @@
             font-family: 'Poppins', system-ui;
         }
     </style>
-
 </head>
 
 <body class="m-0 p-0 w-[100vw] h-[100vh] flex items-center justify-center bg-gray-700">
 <div id="container" class="w-full h-full flex items-center justify-center">
 
-    <div class="modal absolute flex inset-0 bg-black/25 items-center justify-center">
+    <button name="send" class="border-0 bg-blue-600 p-4 rounded-full font-bold text-white shadow-md hover:bg-blue-500" onclick="showPopup()">Compartilhar</button>
+    <?php sharedComponent(); ?>
 
-        <div class="flex flex-col bg-[#1B1B1B] w-auto h-64 rounded-2xl p-6 justify-between shadow-xl">
-            <div class="w-full flex flex-row justify-between items-center">
-                <h2 class="text-white text-xl">Compartilhar</h2>
-    
-                <button onclick="closePopup()" class="flex justify-center items-center w-7 text-purple-400 hover:text-purple-600">
-                    <img src="../../../../public/icons/botaoFechar.svg" class="w-full h-full">
-                </button>
-            </div>
-
-            <div class="flex justify-between">
-                <div id="telegram" class="w-12 h-12 bg-white/5 rounded-full">
-                    <img src="../../../../public/icons/telegram.svg" alt="">
-                </div>
-
-                <div id="facebook" class="w-12 h-12 bg-white/5 rounded-full">
-                    <img src="../../../../public/icons/facebook.svg" alt="">
-                </div>
-
-                <div id="whatsapp" class="w-12 h-12 bg-white/5 rounded-full">
-                    <img src="../../../../public/icons/whats.svg" alt="">
-                </div>
-
-                <div id="instagram" class="w-12 h-12 bg-white/5 rounded-full">
-                    <img src="../../../../public/icons/insta.svg" alt="">
-                </div>
-                
-                <div id="twitter" class="w-12 h-12 bg-white/5 rounded-full">
-                    <img src="../../../../public/icons//twitter.svg" alt="">
-                </div>
-            </div>
-
-            <div class="flex flex-row justify-between gap-3">
-                <input type="text" value="https://youtube.com/@freitasdev" class="focus:outline-none text-gray-500 text-sm p-2 w-72 border-[1px] border-[#666666] bg-transparent rounded-[10px]">
-                <button class="bg-[#6C00C0] p-3 px-7 rounded-[10px] text-white">Copiar</button>
-            </div>
-        </div>
-    
-    </div>
-    
-    <button name="send" class="border-0 bg-blue-600 p-4 rounded-full font-bold text-white shadow-md hover:bg-blue-500" onclick="showPopup()" >Compartilhar</button>
-
-</div>
-
-    <script>
+</div> <!-- # -->
+    <script defer>
         function showPopup() {
             document.querySelector('.modal').classList.remove('hidden');
         }
@@ -81,6 +39,5 @@
             document.querySelector('.modal').classList.add('hidden');
         }
     </script>
-
 </body>
 </html>
