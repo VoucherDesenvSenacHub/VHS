2dc0937bf6e231f094c61934cf4bbae7d61de625
Bruna Gomes <brunagomes9165@gmail.com>
2025-05-19 16:56:20 -0400

    fix: melhorando a qualidade dos ícones da barra lateral

--- src/views/components/barra_lateral/barra_lateral.php
+++ src/views/components/barra_lateral/barra_lateral.php
@@ -5,15 +5,14 @@
 
         echo ' 
         <script src="https://cdn.tailwindcss.com"></script>
-            <body class=" bg-gray-950   ">
-            
-                <aside class="w-[9.25rem] ml-[1.87rem] h-screen">
-                    <h2 class=" pt-[1.18rem] ml-[0.31rem] text-gray-400 text-xs font-poppins">HOME</h2>
+        <script src="../../../styles/tailwindglobal.js"></script>
+                <aside class="w-[16rem] mx-5 fixed h-screen bg-background">
+                    <h2 class=" pt-[1.18rem] ml-[0.31rem] text-gray-400 text-base menu-text font-medium tracking-widest">HOME</h2>
                     <ul>
                         <li class="menu-item flex items-center text-gray-300 rounded-lg cursor-pointer mt-[1.5rem] transition-transform duration-200">
                             <a href="../utils/Inicio.php" class="flex items-center w-full p-2">
-                                <div class="icon home-icon w-[2rem] h-[2rem] flex items-center justify-center bg-opacity-10 bg-white rounded-[12px] ml-[0.31rem] transition-transform duration-200">
-                                    <img src="../../../../public/icons/Home.svg" alt="Início" class="transition-transform duration-200">
+                                <div class="icon home-icon w-10 h-10 flex items-center justify-center bg-opacity-10 bg-white rounded-[12px] ml-[0.31rem] transition-transform duration-200">
+                                    <img src="../../../../public/icons/home.svg" alt="Início" class="transition-transform duration-200">
                                 </div>
                                 <h2 class="menu-text ml-[1rem] text-gray-400 text-sm font-semibold transition-opacity duration-200">Início</h2>
                             </a>
@@ -21,8 +20,8 @@
             
                         <li class="flex items-center text-gray-300 rounded-lg cursor-pointer mt-[2rem]">
                             <a href="../utils/Fast.php" class="flex items-center w-full p-2">
-                                <div class="icon fast-icon w-[2rem] h-[2rem] flex items-center justify-center bg-opacity-10 bg-white rounded-[12px] ml-[0.31rem]">
-                                    <img src="../../../../public/icons/Fast.svg" alt="Fast">
+                                <div class="icon fast-icon w-10 h-10 flex items-center justify-center bg-opacity-10 bg-white rounded-[12px] ml-[0.31rem]">
+                                    <img src="../../../../public/icons/fast.svg" alt="Fast">
                                 </div>
                                 <h2 class="menu-text ml-[1rem] text-gray-400 text-sm font-semibold transition-opacity duration-200">Fast</h2>
                             </a>
@@ -30,8 +29,8 @@
             
                         <li class="flex items-center text-gray-300 rounded-lg cursor-pointer mt-[2rem]">
                             <a href="../utils/Eventos.php" class="flex items-center w-full p-2">
-                                <div class="icon eventos-icon w-[2rem] h-[2rem] flex items-center justify-center bg-opacity-10 bg-white rounded-[12px] ml-[0.31rem]">
-                                    <img src="../../../../public/icons/Eventos.svg" alt="Eventos">
+                                <div class="icon eventos-icon w-10 h-10 flex items-center justify-center bg-opacity-10 bg-white rounded-[12px] ml-[0.31rem]">
+                                    <img src="../../../../public/icons/radio.svg" alt="Eventos">
                                 </div>
                                 <h2 class="menu-text ml-[1rem] text-gray-400 text-sm font-semibold transition-opacity duration-200">Eventos</h2>
                             </a>
@@ -39,23 +38,23 @@
             
                             <li class="flex items-center text-gray-300 rounded-lg cursor-pointer mt-[2rem]">
                             <a href="../utils/Histórico.php" class="flex items-center w-full p-2">
-                                <div class="icon historico-icon w-[2rem] h-[2rem] flex items-center justify-center bg-opacity-10 bg-white rounded-[12px] ml-[0.31rem]">
-                                    <img src="../../../../public/icons/Historico.svg" alt="Histórico">
+                                <div class="icon historico-icon w-10 h-10 flex items-center justify-center bg-opacity-10 bg-white rounded-[12px] ml-[0.31rem]">
+                                    <img src="../../../../public/icons/youtube.svg" alt="Histórico">
                                 </div>
                                 <h2 class="menu-text ml-[1rem] text-gray-400 text-sm font-semibold transition-opacity duration-200">Histórico</h2>
                             </a>
                         </li>
                     </ul>
             
-                    <hr class="divider w-[8.06rem] mt-[1.81rem] border-gray-800 transition-all duration-300 ease-in-out">
+                    <hr class="divider w-52 mt-[1.81rem] border-gray-800 transition-all duration-300 ease-in-out">
             
-                    <h2 id="categoria-title" class="menu-text pt-[1.18rem] ml-[0.31rem] text-gray-400 text-sm font-poppins">CATEGORIA</h2>
+                    <h2 id="categoria-title" class="menu-text pt-[1.18rem] ml-[0.31rem] text-gray-400 text-base menu-text font-medium tracking-widest">CATEGORIA</h2>
             
                     <ul>
                         <li class="flex items-center text-gray-300 rounded-lg cursor-pointer mt-[1.5rem]">
                             <a href="../utils/Tecnologia.php" class="flex items-center w-full p-2">
-                                <div class="icon tecnologia-icon w-[2rem] h-[2rem] flex items-center justify-center bg-opacity-10 bg-white rounded-[12px] ml-[0.31rem]">
-                                    <img src="../../../../public/icons/Tecnologia.svg" alt="Tecnologia">
+                                <div class="icon tecnologia-icon w-10 h-10 flex items-center justify-center bg-opacity-10 bg-white rounded-[12px] ml-[0.31rem]">
+                                    <img src="../../../../public/icons/cpu.svg" alt="Tecnologia">
                                 </div>
                                 <h2 class="menu-text ml-[1rem] text-gray-400 text-sm font-semibold transition-opacity duration-200">Tecnologia</h2>
                             </a>
@@ -63,8 +62,8 @@
             
                         <li class="flex items-center text-gray-300 rounded-lg cursor-pointer mt-[2rem]">
                             <a href="../utils/Saúde.php" class="flex items-center w-full p-2">
-                                <div class="icon saude-icon w-[2rem] h-[2rem] flex items-center justify-center bg-opacity-10 bg-white rounded-[12px] ml-[0.31rem]">
-                                    <img src="../../../../public/icons/Saude.svg" alt="Saúde">
+                                <div class="icon saude-icon w-10 h-10 flex items-center justify-center bg-opacity-10 bg-white rounded-[12px] ml-[0.31rem]">
+                                    <img src="../../../../public/icons/saude.svg" alt="Saúde">
                                 </div>
                                 <h2 class="menu-text ml-[1rem] text-gray-400 text-sm font-semibold transition-opacity duration-200">Saúde</h2>
                             </a>
@@ -72,8 +71,8 @@
             
                         <li class="flex items-center text-gray-300 rounded-lg cursor-pointer mt-[2rem]">
                             <a href="../utils/Moda.php" class="flex items-center w-full p-2">
-                                <div class="icon moda-icon w-[2rem] h-[2rem] flex items-center justify-center bg-opacity-10 bg-white rounded-[12px] ml-[0.31rem]">
-                                    <img src="../../../../public/icons/Moda.svg" alt="Moda">
+                                <div class="icon moda-icon w-10 h-10 flex items-center justify-center bg-opacity-10 bg-white rounded-[12px] ml-[0.31rem]">
+                                    <img src="../../../../public/icons/moda.svg" alt="Moda">
                                 </div>
                                 <h2 class="menu-text ml-[1rem] text-gray-400 text-sm font-semibold transition-opacity duration-200">Moda</h2>
                             </a>
@@ -81,8 +80,8 @@
             
                         <li class="flex items-center text-gray-300 rounded-lg cursor-pointer mt-[2rem]">
                             <a href="../utils/Estética.php" class="flex items-center w-full p-2">
-                                <div class="icon estetica-icon w-[2rem] h-[2rem] flex items-center justify-center bg-opacity-10 bg-white rounded-[12px] ml-[0.31rem]">
-                                    <img src="../../../../public/icons/estetica.svg" alt="Estética">
+                                <div class="icon estetica-icon w-10 h-10 flex items-center justify-center bg-opacity-10 bg-white rounded-[12px] ml-[0.31rem]">
+                                    <img src="../../../../public/icons/Estetica.svg" alt="Estética">
                                 </div>
                                 <h2 class="menu-text ml-[1rem] text-gray-400 text-sm font-semibold transition-opacity duration-200">Estética</h2>
                             </a>
@@ -91,9 +90,7 @@
             
                     <hr class="divides w-[8.06rem] mt-[1.81rem] border-gray-800 transition-all duration-300 ease-in-out">
                 </aside>
-            </body>
             
             <script src="script.js"></script>
         ';
     }
-BarraLateralComponent();
