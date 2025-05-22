b09a47416e5e0b24e2ee679ac48873f185ca73dd
LuanEnrico30 <luanzinhoenrico@gmail.com>
2025-05-22 14:46:06 -0400

    Ref: Fiz essas páginas em mobile e arrumei o tamanho da logo cassete e depois arrumei o branco na página quando dava zoom

--- src/views/auth/interests/index.php
+++ src/views/auth/interests/index.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace Src\Views\Components\Utils;
 
 require "../../components/utils/buttonComponent.php";
@@ -7,7 +6,6 @@ require "../../components/utils/inputComponent.php";
 
 use function Src\Views\Components\Utils\InputComponent;
 use function Src\Views\Components\Utils\ButtonComponent;
-
 ?>
 
 <!DOCTYPE html>
@@ -15,35 +13,31 @@ use function Src\Views\Components\Utils\ButtonComponent;
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>interests</title>
+    <title>Interests</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="../../../styles/tailwindglobal.js"></script>
 </head>
-
 <body>
-    <div class="flex h-screen bg-background text-white">
-        <div class="flex justify-center mr-24">
+    <div class="flex min-h-screen bg-background text-white xl:justify-start justify-center">
+        <div class="flex justify-center mr-24 xl:mr-24 max-xl:hidden">
             <img src="../../../../public/images/Cassete.svg" alt="" class="relative right-14 mr-28 w-6/16">
         </div>
-
-      <div class="relative mx-20 w-1/2 flex items-center justify-center shadow-[-10px_0_30px_10px_rgba(255, 255, 255, 1)]">
-        <div class="flex flex-col gap-4">
-            <div class="flex items-center flex-col gap-2">
-                <img src="../../../../public/logos/Logo.svg" alt="">
-                <p class="text-4xl font-pop font-semibold title-size text-white">Selecione seus interesses</p>
-                <p class="font-pop paragraph-size text-gray-200">Receba sugestão de vídeos relacionados.</p>
-            </div>
-            <div class="flex flex-col gap-4 w-96">
-                        <?= ButtonComponent(text: "Estética", variant: "outline") ?>
-                        <?= ButtonComponent(text: "Saúde", variant: "outline") ?>
-                        <?= ButtonComponent(text: "Tecnologia", variant: "outline") ?>
-                        <?= ButtonComponent(text: "Moda", variant: "outline") ?>
-                        <?= ButtonComponent(text: "Continuar", variant: "default") ?>
-                    </div>
+        <div class="relative xl:min-w-1/2 xl:mx-20 flex items-center justify-center w-full max-w-md xl:max-w-none xl:w-auto px-4 xl:px-0">
+            <div class="flex flex-col gap-4">
+                <div class="flex items-center flex-col gap-2">
+                    <img src="../../../../public/logos/Logo.svg" alt="">
+                    <p class="text-4xl font-pop font-semibold title-size text-white">Selecione seus interesses</p>
+                    <p class="font-pop paragraph-size text-gray-200">Receba sugestão de vídeos relacionados.</p>
+                </div>
+                <div class="flex flex-col gap-4 ml-4 w-full xl:w-96">
+                    <?= ButtonComponent(text: "Estética", variant: "outline") ?>
+                    <?= ButtonComponent(text: "Saúde", variant: "outline") ?>
+                    <?= ButtonComponent(text: "Tecnologia", variant: "outline") ?>
+                    <?= ButtonComponent(text: "Moda", variant: "outline") ?>
+                    <?= ButtonComponent(text: "Continuar", variant: "default") ?>
                 </div>
             </div>
         </div>
     </div>
 </body>
-
 </html>
