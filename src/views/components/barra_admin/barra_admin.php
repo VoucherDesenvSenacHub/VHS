f2fff98bdee7783e9056360aab7ff67b00a8d322
João Augusto Cintra Campos <joaoaugustocintracampos@gmail.com>
2025-05-22 15:09:59 -0400

    feat-barra-admin

--- src/views/components/barra_admin/barra_admin.php
+++ src/views/components/barra_admin/barra_admin.php
@@ -1,51 +1,40 @@
 <?php
-function barra_admin(){
-    return('
-        <aside class="w-36 ml-8 h-screen">
+namespace src\views\components\header;
 
-            <h2 class=" pt-5 ml-1 text-gray-400 text-xs font-poppins">ADMINISTRAÇÃO</h2>
+function isActive($page)
+{
+    return basename($_SERVER['PHP_SELF']) === $page ? 'bg-[#660BAD] bg-opacity-100 text-white' : '';
+}
 
-            <ul>
+$menuItems = [
+    'Analytics.php' => ['icon' => 'chart.svg', 'label' => 'Analytics', 'class' => 'analytics-icon'],
+    'usuarios.php' => ['icon' => 'group.svg', 'label' => 'Usuários', 'class' => 'usuarios-icon'],
+];
+
+function barra_admin() {
+    global $menuItems;
+
+    echo '<div id="sideMenuContainer">';
+    echo '<aside class="w-[9.25rem] ml-[1.87rem] h-screen">';
+    echo '<h2 class="pt-[1.18rem] ml-[0.31rem] text-gray-400 text-xs font-poppins">ADMINISTRAÇÃO</h2><ul>';
+
+    foreach ($menuItems as $page => $data) {
+        echo '<li class="flex items-center text-gray-300 rounded-lg cursor-pointer mt-[1.5rem]">
+            <a href="/VHS/src/views/auth/'.$page.'" class="flex items-center w-full p-2">
+                <div class="icon w-[2rem] h-[2rem] flex items-center justify-center bg-[#660BAD] '.(isActive($page) ? 'bg-opacity-100' : 'bg-opacity-10').' rounded-[12px] ml-[0.31rem] '.$data['class'].'">
+                    <img src="/VHS/public/icons/sidebar_admin/'.$data["icon"].'" alt="'.$data["label"].'" class="'.(isActive($page) ? 'filter invert brightness-0' : '').'">
+                </div>
+                
+                <h2 class="menu-text ml-[1rem] text-gray-400 text-sm font-semibold ' .(isActive($page) ? 'text-white' : '').'" style="display: none;" >'.$data["label"].'</h2>
+
+            </a>
+        </li>';
+    }
+
     
-                <li class="flex items-center text-gray-300 rounded-lg cursor-pointer mt-8">
 
-                    <a href="./analytics.php" class="flex items-center w-full p-2">
+    echo '</ul></aside></div>';
 
-                        <div class="icon analytics-icon w-8 h-8 flex items-center justify-center bg-opacity-10 bg-white rounded-xl ml-1">
-
-                            <img src="/VHS/public/images/chart.svg" alt="analytics">
-
-                        </div>
-
-                        <h2 class="menu-text ml-4 text-gray-400 text-sm font-semibold transition-opacity duration-200">Analytics</h2>
-
-                    </a>
-
-                </li>
- 
-                <li class="flex items-center text-gray-300 rounded-lg cursor-pointer mt-8">
-
-                    <a href="./usuarios.php" class="flex items-center w-full p-2" class="flex items-center w-full p-2">
-
-                        <div class="icon usuarios-icon w-8 h-8 flex items-center justify-center bg-opacity-10 bg-white rounded-xl ml-1">
-
-                            <img src="/VHS/public/images/group.svg" alt="usuários">
-
-                        </div>
-
-                        <h2 class="menu-text ml-4 text-gray-400 text-sm font-semibold transition-opacity duration-200">Usuários</h2>
-
-                    </a>
-
-                </li>
-
-            </ul>
-            
-            <script src="script.js"></script>
-
-            '
-            
-    );
-
+    echo '<script src="/VHS/src/views/components/barra_admin/barra_admin.js"></script>';
 }
 ?>
