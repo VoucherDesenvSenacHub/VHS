b5e10cc52d392d7ee08baca7bab805528682f8f1
Josué <josuealexandre067@gmail.com>
2025-06-05 17:10:47 -0400

    *****************ryryryryryyryryry************///////////\/\\/\/\/\/\/\/\//\/\//aaaaaaaaaaaaaaaaaaazzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzfeatfeatfeatfeatfeat/************

---
+++ src/views/components/utils/exampleView.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>userMenuComponent</title>
+    <link rel="stylesheet" href="../../../styles/global.css">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+</head>
+
+<body class="flex items-center justify-center min-h-screen bg-[#0C0118]">
+
+    <?php include './userMenuComponent.php'; ?>
+
+</body>
+</html>
