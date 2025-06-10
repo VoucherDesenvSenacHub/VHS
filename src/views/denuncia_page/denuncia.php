49997fca6961e69b2de7f402d9ec17ec66dccd19
Pedro Celestino <pedrojuniorcelestinocg@gmail.com>
2025-06-10 16:59:52 -0400

    feat: ta lá

---
+++ src/views/denuncia_page/denuncia.php
@@ -0,0 +1,77 @@
+<?php
+require "../components/header/headerComponent.php";
+require "../components/barra_admin/barra_admin.php";
+require "../components/utils/inputComponent.php";
+require "../components/utils/cardDenunciationComponent.php";
+require "../components/utils/coment_admin/comentAdmin.component.php";
+
+use function Src\Views\Components\header\HeaderComponent;
+use function src\views\components\header\Barra_Admin;
+use function Src\Views\Components\Utils\Comment;
+
+$commets_lista = [
+    [   "name" => "Rafael",
+        "id" => "1234",
+        "text" => "cara muuit legal odio todos vsz seus caras chtos",
+        "thumbnail_url" => "https://www.uai.com.br/uainoticias/wp-content/uploads/2025/04/Ornitorrinco_1744132096128.jpg","há 2 dias","https://www.uai.com.br/uainoticias/wp-content/uploads/2025/04/Ornitorrinco_1744132096128.jpg",
+        "created_at" => "há 2 dias",
+        "user" => "https://www.uai.com.br/uainoticias/wp-content/uploads/2025/04/Ornitorrinco_1744132096128.jpg","há 2 dias","https://www.uai.com.br/uainoticias/wp-content/uploads/2025/04/Ornitorrinco_1744132096128.jpg"
+    ]
+];
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>user management</title>
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script src="../../styles/tailwindglobal.js"></script>
+    </head>
+    <body class="w-full bg-[#0C0118]">
+    
+    <?php HeaderComponent();?>
+    <div class="flex">
+        <div class="max-xl:hidden mr-4">
+            <?php Barra_Admin(); ?>
+        </div>
+        <div class="p-7 w-full">
+            <div>
+                <p class="font-pop font-semibold text-title text-white">Gerenciamento de usuários</p>
+                <p class="text-subtitile font-semibold title-size text-gray-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pellentesque elit nisl,</p>
+            </div>
+            <div class="flex h-5 mt-5 gap-5">
+                <a class="text-white bg-gray600 rounded-full w-28 h-8 flex items-center justify-center">Usuários</a>
+                <a href="../Perfil_Empresa.php" class="text-gray-500 bg-gray600 rounded-full w-28 h-8 flex items-center justify-center">Denúncias</a>
+            </div>
+            <div class="mt-10 flex items-center gap-4 h-16 cavalo">
+                <button id="btn_filter" onclick="filter(event)">
+                    <img class="size-7" src="../../../public/icons/filter-svgrepo-com.svg" alt="">
+                </button>
+                <input type="text" placeholder="Pesquisar" class="pl-2 rounded-lg bg-transparent text-white w-full h-12 border-[0.5px] border-gray-500">
+            </div>
+            <div id="filter" class="absolute left-[16.5rem] z-10 hidden flex flex-col bg-gray-900 rounded-lg p-2 max-w-32 border-[0.5px] border-gray-500">
+                <div class="flex">
+                    <img src="../../../public/icons/time-svgrepo-com.svg" alt="" class="size-6 rotate-[-110deg]">
+                    <p class="text-[13px] flex items-center text-gray-200">Mais recentes</p>
+                </div>
+                <div class="flex flex-row">
+                    <img src="../../../public/icons/time-svgrepo-com.svg" class="size-6" alt="">
+                    <p class="text-[13px] flex items-center text-gray-200">Mais antigos</p>
+                </div>
+            </div>
+            <div class="colocaraqui mt-10 flex flex-col gap-4">
+            
+                <?php
+                    echo Comment("RAFAEL","MUITO TEXTO MESMO MESMO","https://www.uai.com.br/uainoticias/wp-content/uploads/2025/04/Ornitorrinco_1744132096128.jpg","há 2 dias","https://www.uai.com.br/uainoticias/wp-content/uploads/2025/04/Ornitorrinco_1744132096128.jpg");
+                ?>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
