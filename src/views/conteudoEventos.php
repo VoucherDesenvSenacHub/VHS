681c31170e41198716fd1e5324352f9da3a0f9dc
Pedro Celestino <pedrojuniorcelestinocg@gmail.com>
2025-06-16 15:25:36 -0400

    Update: falta cards e barra lateral corrigidos

---
+++ src/views/conteudoEventos.php
@@ -0,0 +1,196 @@
+<?php
+require "./components/sidebar/barra_lateral.php";
+require "./components/header/headerComponent.php";
+require_once "./components/cards/myCard.php";
+require_once "./components/cards/formatCard.php";
+require_once "./components/studioSideMenu/studioSideMenuComponent.php";
+
+use function Src\Views\Components\header\HeaderComponent;
+use function src\views\components\header\isActive;
+use function src\views\components\header\StudioSideMenuComponent;
+use function src\views\components\sidebar\SidebarComponent;
+
+$videos = [
+    [
+        "url" => "https://youtube.com/@freitasdev",
+        "type_card" => "Evento",
+        "description" => "Gratuito",
+        "duration" => 360,
+        "title" => "Como aprender programação do zero e se tornar um excelente desenvolvedor full stack",
+        "username" => "Canal Dev",
+        "thumbnail_url" => "https://t.ctcdn.com.br/69rFkwz-cdviPGZn2p_l6rJH0UA=/1200x675/smart/i533291.png",
+        "avatar_url" => "https://cdn.awsli.com.br/10/10790/produto/292478529/fix-copo-bola-foto-1-7hxddc8b9q.jpg",
+        "account_type" => "verified",
+        "views" => 1250000,
+        "created_at" => "2024-03-10 15:00:00"
+    ],
+    [
+        "url" => "https://youtube.com/@techworld",
+        "type_card" => "Tutorial",
+        "description" => "Pago",
+        "duration" => 600,
+        "title" => "Construindo uma API REST com Node.js e Express",
+        "username" => "Tech World",
+        "thumbnail_url" => "https://img.youtube.com/vi/xyz123/maxresdefault.jpg",
+        "avatar_url" => "https://example.com/avatar/techworld.jpg",
+        "account_type" => "verified",
+        "views" => 850000,
+        "created_at" => "2024-05-15 10:30:00"
+    ],
+    [
+        "url" => "https://youtube.com/@codeacademy",
+        "type_card" => "Curso",
+        "description" => "Gratuito",
+        "duration" => 1200,
+        "title" => "Introdução ao Machine Learning com Python",
+        "username" => "Code Academy",
+        "thumbnail_url" => "https://img.youtube.com/vi/abc456/maxresdefault.jpg",
+        "avatar_url" => "https://example.com/avatar/codeacademy.jpg",
+        "account_type" => "padrão",
+        "views" => 230000,
+        "created_at" => "2024-01-20 14:00:00"
+    ],
+    [
+        "url" => "https://youtube.com/@devtutoriais",
+        "type_card" => "Webinar",
+        "description" => "Gratuito",
+        "duration" => 900,
+        "title" => "Desenvolvimento de Aplicativos Mobile com Flutter",
+        "username" => "Dev Tutoriais",
+        "thumbnail_url" => "https://img.youtube.com/vi/def789/maxresdefault.jpg",
+        "avatar_url" => "https://example.com/avatar/devtutoriais.jpg",
+        "account_type" => "verified",
+        "views" => 450000,
+        "created_at" => "2024-06-01 09:00:00"
+    ],
+    [
+        "url" => "https://youtube.com/@programacaofacil",
+        "type_card" => "Tutorial",
+        "description" => "Gratuito",
+        "duration" => 240,
+        "title" => "Aprenda HTML e CSS em 4 horas",
+        "username" => "Programação Fácil",
+        "thumbnail_url" => "https://img.youtube.com/vi/ghi012/maxresdefault.jpg",
+        "avatar_url" => "https://example.com/avatar/programacaofacil.jpg",
+        "account_type" => "padrão",
+        "views" => 320000,
+        "created_at" => "2024-04-25 16:45:00"
+    ],
+    [
+        "url" => "https://youtube.com/@codinglife",
+        "type_card" => "Evento",
+        "description" => "Pago",
+        "duration" => 1800,
+        "title" => "Workshop de DevOps com Docker e Kubernetes",
+        "username" => "Coding Life",
+        "thumbnail_url" => "https://img.youtube.com/vi/jkl345/maxresdefault.jpg",
+        "avatar_url" => "https://example.com/avatar/codinglife.jpg",
+        "account_type" => "verified",
+        "views" => 670000,
+        "created_at" => "2023-12-10 11:00:00"
+    ],
+    [
+        "url" => "https://youtube.com/@techtalks",
+        "type_card" => "Palestra",
+        "description" => "Gratuito",
+        "duration" => 720,
+        "title" => "O Futuro da Inteligência Artificial em 2025",
+        "username" => "Tech Talks",
+        "thumbnail_url" => "https://img.youtube.com/vi/mno678/maxresdefault.jpg",
+        "avatar_url" => "https://example.com/avatar/techtalks.jpg",
+        "account_type" => "verified",
+        "views" => 980000,
+        "created_at" => "2024-02-15 13:20:00"
+    ],
+    [
+        "url" => "https://youtube.com/@webdevpro",
+        "type_card" => "Tutorial",
+        "description" => "Gratuito",
+        "duration" => 480,
+        "title" => "Construindo um Blog com React e Firebase",
+        "username" => "Web Dev Pro",
+        "thumbnail_url" => "https://img.youtube.com/vi/pqr901/maxresdefault.jpg",
+        "avatar_url" => "https://example.com/avatar/webdevpro.jpg",
+        "account_type" => "padrão",
+        "views" => 150000,
+        "created_at" => "2024-05-30 08:15:00"
+    ],
+    [
+        "url" => "https://youtube.com/@learncode",
+        "type_card" => "Curso",
+        "description" => "Pago",
+        "duration" => 1500,
+        "title" => "Dominando Bancos de Dados com SQL e NoSQL",
+        "username" => "Learn Code",
+        "thumbnail_url" => "https://img.youtube.com/vi/stu234/maxresdefault.jpg",
+        "avatar_url" => "https://example.com/avatar/learncode.jpg",
+        "account_type" => "verified",
+        "views" => 500000,
+        "created_at" => "2023-11-05 17:30:00"
+    ]
+];
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>user management</title>
+        <script src="https://cdn.tailwindcss.com"></script>
+        <script src="../../styles/tailwindglobal.js"></script>
+    </head>
+    <body class="w-full bg-[#0C0118]">
+   
+
+        <?php echo HeaderComponent();?>
+    <div class="flex">
+        <div class="max-xl:hidden mr-4">
+            <?php  
+                echo StudioSideMenuComponent();
+            ?>
+        </div>
+        <div class="p-7 w-full">
+            <div>
+                <p class="font-pop font-semibold text-title text-white">Gerenciamento de usuários</p>
+                <p class="text-subtitile font-semibold title-size text-gray-300">Lorem ipsum dolor sit amet consectetur adipisicing elit. Pellentesque elit nisl,</p>
+            </div>
+            <div class="flex h-5 mt-5 gap-5">
+                <a class="text-[#C4C4C4] bg-[#202024] rounded-full w-28 h-8 flex items-center justify-center">Videos</a>
+                <a class="text-[#C4C4C4] bg-[#202024] rounded-full w-28 h-8 flex items-center justify-center">Fast</a>
+                <a class="text-white bg-[#202024] rounded-full w-28 h-8 flex items-center justify-center">Eventos</a>
+            </div>
+            <div class="mt-10 flex items-center gap-4 h-16 cavalo">
+                <button id="btn_filter" onclick="filter(event)">
+                    <img class="size-7" src="/VHS/public/icons/filter-svgrepo-com.svg" alt="">
+                </button>
+                <input type="text" placeholder="Pesquisar" class="pl-2 rounded-lg bg-transparent text-white w-full h-12 border-[0.5px] border-gray-500">
+            </div>
+            <div id="filter" class="absolute left-[16.5rem] z-10 hidden flex flex-col bg-gray-900 rounded-lg p-2 max-w-32 border-[0.5px] border-gray-500">
+                <div class="flex">
+                    <img src="../../../public/icons/time-svgrepo-com.svg" alt="" class="size-6 rotate-[-110deg]">
+                    <p class="text-[13px] flex items-center text-gray-200">Mais recentes</p>
+                </div>
+                <div class="flex flex-row">
+                    <img src="../../../public/icons/time-svgrepo-com.svg" class="size-6" alt="">
+                    <p class="text-[13px] flex items-center text-gray-200">Mais antigos</p>
+                </div>
+            </div>
+            <div class="colocaraqui mt-10 flex flex-col gap-4 max-w-[1440px]">
+                <div class="flex flex-wrap gap-10">
+                    <?php
+                        foreach($videos as $video){
+                            
+                            echo createMyCard($video);
+                            
+                        }
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+</html>
