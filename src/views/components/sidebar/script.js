c352eff314ef02dd477f3bb920120b0c56baa2b3
Kayke Adriano <kayke.adrianobdg@gmail.com>
2025-06-17 16:59:44 -0400

    feat da barra

--- src/views/components/sidebar/script.js
+++ src/views/components/sidebar/script.js
@@ -10,30 +10,31 @@ document.addEventListener("DOMContentLoaded", function () {
     toggleButton.addEventListener("click", function () {
         isExpanded = !isExpanded;
 
+        sidebar.style.transition = "width 0.3s ease-in-out, margin-left 0.3s ease-in-out";
         sidebar.style.width = isExpanded ? "9.25rem" : "3.5rem";
         sidebar.style.marginLeft = isExpanded ? "1.87rem" : "0.5rem";
 
         texts.forEach(text => {
+            text.style.transition = "opacity 0.3s ease-in-out";
             text.style.display = isExpanded ? "block" : "none";
             text.style.opacity = isExpanded ? "1" : "0";
-            text.style.transition = "opacity 500ms ease-in-out";
         });
 
+        divider.style.transition = "width 0.3s ease-in-out";
         divider.style.width = isExpanded ? "8.06rem" : "2.5rem";
-        divider.style.transition = "width 500ms ease-in-out";
 
+        divides.style.transition = "width 0.3s ease-in-out";
         divides.style.width = isExpanded ? "8.06rem" : "2.5rem";
-        divides.style.transition = "width 500ms ease-in-out";
 
+        categoriaTitle.style.transition = "opacity 0.3s ease-in-out";
         categoriaTitle.style.display = isExpanded ? "block" : "none";
 
         const seta = document.querySelector("#seta");
-        const path = seta.querySelector("path");
-
-        if (isExpanded) {
-            path.setAttribute("d", "M24.8995 16.2329C25.6972 16.6324 25.6972 17.7708 24.8995 18.1703L23.1524 19.0451C22.432 19.4059 21.584 18.8821 21.584 18.0765L21.584 16.3267C21.584 15.5211 22.432 14.9974 23.1524 15.3581L24.8995 16.2329Z");
-        } else {
-            path.setAttribute("d", "M3.1005 16.2329C2.3028 16.6324 2.3028 17.7708 3.1005 18.1703L4.8476 19.0451C5.568 19.4059 6.416 18.8821 6.416 18.0765L6.416 16.3267C6.416 15.5211 5.568 14.9974 4.8476 15.3581L3.1005 16.2329Z");
+        const path = seta?.querySelector("path");
+        if (path) {
+            path.setAttribute("d", isExpanded
+                ? "M24.8995 16.2329C25.6972 16.6324 25.6972 17.7708 24.8995 18.1703L23.1524 19.0451C22.432 19.4059 21.584 18.8821 21.584 18.0765L21.584 16.3267C21.584 15.5211 22.432 14.9974 23.1524 15.3581L24.8995 16.2329Z"
+                : "M3.1005 16.2329C2.3028 16.6324 2.3028 17.7708 3.1005 18.1703L4.8476 19.0451C5.568 19.4059 6.416 18.8821 6.416 18.0765L6.416 16.3267C6.416 15.5211 5.568 14.9974 4.8476 15.3581L3.1005 16.2329Z");
         }
     });
 
