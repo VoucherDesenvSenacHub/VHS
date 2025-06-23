19ecf2913515ab72490e67f50d3539550f14df1e
Ronan <ronanscampos@hotmail.com>
2025-06-23 17:06:19 -0400

    UPDATE: fix media query view_Customizar

--- src/views/studio/Customizar.php
+++ src/views/studio/Customizar.php
@@ -26,19 +26,19 @@ use function src\views\components\Utils\Footer;
     <?php echo StudioSideMenuComponent(); ?>
 
     <!-- Conteúdo da Página -->
-    <main class="flex-1 p-8 md:p-12 ml-10">
+    <main class="flex flex-col p-8 md:p-12 ml-14 w-4/8">
       <h1 class="text-2xl font-semibold mb-6">Customizar canal</h1>
       <p class="text-sm text-gray-300 mb-8">Altere o nome, banner, foto de perfil, descrição do seu canal</p>
 
       <!-- Foto de Perfil -->
-      <section class="mb-10 flex gap-5">
+      <section class="mb-10 flex flex-col lg:flex-row gap-5">
         <form action="upload.php" method="POST" enctype="multipart/form-data">
-          <label for="imagemUpload" class="relative group cursor-pointer inline-block">
+          <label for="imagemUpload" class="relative group cursor-pointer inline-block w-45 h-45">
             <!-- Imagem de perfil atual -->
             <img
               src="https://media.istockphoto.com/id/1316134499/pt/foto/a-concept-image-of-a-magnifying-glass-on-blue-background-with-a-word-example-zoom-inside-the.jpg?s=612x612&w=0&k=20&c=raTXPP4qnJy_svR1J6dOYeoonbJOWeezfvGd9mAE5vo="
               alt="Foto de perfil"
-              class="w-28 h-28 rounded-xl object-cover group-hover:brightness-75 transition"
+              class="w-45 h-45 rounded-xl object-cover group-hover:brightness-75 transition"
             />
             <!-- Ícone de upload -->
             <div class="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition">
@@ -57,7 +57,7 @@ use function src\views\components\Utils\Footer;
         </form>
         <div class="mt-5">
           <h2 class="text-lg font-medium mb-2">Foto de perfil</h2>
-          <p class="text-sm text-gray-300 mb-8">Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+          <p class="text-sm text-gray-300 mb-8">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis, voluptas dicta! Doloremque nemo neque voluptates, officia commodi recusandae adipisci beatae, inventore quod itaque iure quam aliquid deleniti facere optio accusantium.</p>
         </div>
       </section>
 
@@ -66,32 +66,56 @@ use function src\views\components\Utils\Footer;
       <section class="mb-10">
         <h2 class="text-lg font-medium mb-2">Banner do canal</h2>
         <p class="text-sm text-gray-400 mb-2">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        <img src="https://i.imgur.com/UvnbC0y.png" alt="Banner do canal" class="h-50 w-full max-w-4xl rounded-md border border-gray-700" />
+        <form action="upload.php" method="POST" enctype="multipart/form-data">
+          <label for="imagemUpload" class="relative group cursor-pointer inline-block w-full">
+            <!-- Imagem de perfil atual -->
+            <img src="https://i.imgur.com/UvnbC0y.png" alt="Banner do canal" class="h-50 w-full rounded-md border border-gray-700" />
+            <!-- Ícone de upload -->
+            <div class="absolute inset-0 flex items-center justify-center opacity-0 group-hover:opacity-100 transition">
+              <img src="/VHS/public/icons/Download.svg" alt="Download">
+            </div>
+            <!-- Input invisível -->
+            <input
+              type="file"
+              name="imagem"
+              id="imagemUpload"
+              accept="image/*"
+              class="hidden"
+              required
+            />
+          </label>
+        </form>
       </section>
 
       <!-- Nome do canal -->
-      <section class="mb-6">
+    <form action="" class="flex flex-col">
+    <section class="mb-6">
         <label for="nome" class="block text-sm mb-1">Nome do canal</label>
-        <input id="nome" type="text" class="w-full max-w-xl bg-transparent border border-gray-600 text-sm px-4 py-2 rounded-md placeholder-gray-500" placeholder="@Rafael__">
+        <input id="nome" type="text" class="w-full bg-transparent border border-gray-600 text-sm px-4 py-2 rounded-md placeholder-gray-500" placeholder="@Rafael__">
       </section>
 
       <!-- Descrição -->
       <section class="mb-6">
         <label for="descricao" class="block text-sm mb-1">Descrição</label>
-        <textarea id="descricao" rows="5" class="w-full max-w-xl bg-transparent border border-gray-600 text-sm px-4 py-2 rounded-md placeholder-gray-500" placeholder="Escreva algo sobre o canal..."></textarea>
+        <textarea id="descricao" rows="5" class="w-full bg-transparent border border-gray-600 text-sm px-4 py-2 rounded-md placeholder-gray-500" placeholder="Escreva algo sobre o canal..."></textarea>
       </section>
 
       <!-- Tags do canal -->
       <section class="mb-10">
         <label for="tags" class="block text-sm mb-1">Tags do canal</label>
-        <input id="tags" type="text" class="w-full max-w-xl bg-transparent border border-gray-600 text-sm px-4 py-2 rounded-md placeholder-gray-500" placeholder="#Tecnologia">
+        <input id="tags" type="text" class="w-full bg-transparent border border-gray-600 text-sm px-4 py-2 rounded-md placeholder-gray-500" placeholder="#Tecnologia">
       </section>
 
       <!-- Botões -->
-      <div class="flex gap-4">
-        <button class="px-6 py-2 border border-gray-500 text-sm rounded-md hover:bg-gray-700 transition">Cancelar</button>
-        <button class="px-6 py-2 bg-[#A347FF] text-white text-sm rounded-md hover:bg-[#922be8] transition">Salvar alterações</button>
+      <div class="flex gap-5 flex-col lg:flex-row">
+        <button class="px-4 sm:px-6 md:px-8 lg:px-[10.5rem] py-3 border border-gray-500 text-xs sm:text-sm rounded-md hover:bg-gray-700 transition">
+          Cancelar
+        </button>
+        <button class="px-4 sm:px-6 md:px-8 lg:px-[9.1rem] py-3 bg-[#A347FF] text-white text-xs sm:text-sm rounded-md hover:bg-[#922be8] transition">
+          Salvar alterações
+        </button>
       </div>
+    </form>
     </main>
   </div>
 
