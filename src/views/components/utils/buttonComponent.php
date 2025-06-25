00166f791863105f2bfb4616d674bac1580f9751
Ronan <ronanscampos@hotmail.com>
2025-06-25 14:45:33 -0400

    refactor: add link to buttonComponent

--- src/views/components/utils/buttonComponent.php
+++ src/views/components/utils/buttonComponent.php
@@ -2,7 +2,7 @@
     
     namespace Src\Views\Components\Utils;
 
-    function ButtonComponent(string $text, string $variant ,string $icon = null, string $width = "380px", string $height = "50px", $id=null) {
+    function ButtonComponent(string $text, string $variant ,string $icon = null, string $width = "380px", string $height = "50px", $id=null, string $link) {
         
         $icon = $icon ? "<img src='$icon' class='w-4 h-4'>" : "";
 
@@ -34,6 +34,7 @@
                 <button id='$id' class='$button_style'>
                     $icon
                     $text
+                    <a href='$link'></a>
                 </button>
             "
         ); 
