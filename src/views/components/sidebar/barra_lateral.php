6b296152b645b9bbc76e1a175605a9c0a7f258ef
____x_ <josuealexandre067@gmail.com>
2025-06-27 23:28:37 -0400

    feat: trazendo arquivos para a nova branch

--- src/views/components/sidebar/barra_lateral.php
+++ src/views/components/sidebar/barra_lateral.php
@@ -3,7 +3,6 @@
     namespace Src\Views\Components\Sidebar;
 
     function SidebarComponent() {
-<<<<<<< Updated upstream
         return '  
             <aside class="w-[9.25rem] ml-[1.87rem] h-screen transition-all duration-500 ease-in-out backdrop-blur-sm" id="sidebar">
                 <h2 class="pt-[1.18rem] ml-[0.31rem] text-gray-400 text-xs font-poppins">HOME</h2>
@@ -12,18 +11,6 @@
                         <a href="/VHS/utils/Inicio.php" class="flex items-center w-full p-2">
                             <div class="icon home-icon w-[2rem] h-[2rem] flex items-center justify-center bg-white/5 rounded-[12px] ml-[0.31rem] ">
                                 <img src="/VHS/public/icons/Home.svg" alt="Início">
-=======
-        return "
-            <aside class='flex w-[9.25rem] ml-[1.25rem] mr-[1.25rem] transition-all duration-500 ease-in-out top-[4.5rem] sticky z-10 h-full justify-center flex-col select-none' id='sidebar'>
-
-                <h2 id='home-title' class='menu-text p-2 pt-0 text-gray-400 text-xs 2xl:text-sm font-poppins'>MENU</h2>
-                
-                <ul id='ul-menu' class='flex flex-col gap-[1.5rem] 2xl:gap-[2.5rem] mt-[1rem] 2xl:mt-[1.5rem] justify-center pl-2 pr-2'>
-                    <li class='menu-item flex items-center text-gray-300 rounded-lg cursor-pointer transition-transform duration-200'>
-                        <a href='#' class='flex items-center w-full'>
-                            <div class='icon home-icon w-[2rem] h-[2rem] flex items-center justify-center bg-white/5 rounded-[12px]  '>
-                                <img src='/VHS/public/icons/Home.svg' alt='Início'>
->>>>>>> Stashed changes
                             </div>
                             <h2 class="menu-text ml-[1rem] text-gray-400 text-sm font-semibold transition-all duration-500 ease-in-out">Início</h2>
                         </a>
