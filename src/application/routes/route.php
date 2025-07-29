925fc047a8d3145c90258983adb5a5602f85226d
Guilherme Freitas do Nascimento <121752740+SrFreitass@users.noreply.github.com>
2025-07-29 14:25:37 -0400

    feat: estruturação do back-end (MVC)

--- src/application/routes/route.php
+++ src/application/routes/route.php
@@ -1,16 +1,12 @@
 <?php
 
 require_once __DIR__ . '/../../application/routes/route.config.php';
-require_once __DIR__ . '/../../vendor/routes.autoload.php';
 require_once __DIR__ . '/../../../vendor/autoload.php';
-require_once __DIR__ . '/../../controllers/listVideo.controller.php';
-require_once __DIR__ . '/../../controllers/listVideoCategory.controller.php';
+require_once __DIR__ . '/../../controllers/signUp.controller.php';
 
 use Dotenv\Dotenv;
-use Src\Application\Routes\Router;
 use Src\Application\Controllers\SignUpController;
-use Src\Application\Controllers\ListVideoController;
-use Src\Application\Controllers\ListVideoCategoryController;
+use Src\Application\Routes\Router;
 
 $dotenv = Dotenv::createImmutable(__DIR__ . "/../../..");
 $dotenv->load();
@@ -18,10 +14,6 @@ $dotenv->load();
 $router = new Router();
 
 $router->post('/api/v1/auth/signup', SignUpController::class);
-$router->post('/api/v1/auth/verify-email', VerifyEmailController::class);
-$router->get('/api/v1/videos/popular', ListVideoController::class);
-$router->get('/api/v1/videos/popularCategory', ListVideoCategoryController::class);
-
 
 $router->run();
 
--- src/controllers/signUp.controller.php
+++ src/controllers/signUp.controller.php
@@ -2,189 +2,49 @@
 
 namespace Src\Application\Controllers;
 
-use Src\Application\Utils\EmailTransporter;
+use Respect\Validation\Exceptions\NestedValidationException;
+use Src\Application\Core\Controller;
 use Src\Infra\Models\UserModel;
 
-use function Src\Application\Utils\verifyRecaptcha;
-require_once __DIR__ . '/../application/utils/verifyRecaptcha.php';
-require_once __DIR__ . '/../application/utils/emailTransporter.php';
-require_once __DIR__ . '/../infra/models/userModel.php';
+use Respect\Validation\Validator as v;
 
+require_once __DIR__ . '/../application/core/controller.php';
 
-class SignUpController {
-    public function handle() {
-        
+class SignUpController extends Controller {
+    public UserModel $userModel;
+
+    public function index() {
         try {
-            if(!isset($_POST["email"]) || !isset($_POST["name"]) || !isset($_POST["date_birthday"]) || !isset($_POST["password"]) || !isset($_POST["password_confirmation"]) || !isset($_POST["token_recaptcha"])) {
-                http_response_code(400);
-                return [
-                    "success" => false,
-                    "errors" => [
-                        "error" => true,
-                        "missing_fields" => [
-                            "error_code" => 1,
-                            "message" => "Campos obrigatórios não preenchidos"
-                        ]
-                    ]
-                ];
-            }
+            $this->userModel = $this->model("user");
+
+            $schema = 
+            v::key(
+                'name',
+                v::stringType()->length(3, 150)
+            )->key(
+                'email',
+                v::email(),
+            )->key(
+                'password',
+                v::stringType()->length(8, 16)
+            )->key(
+                'date_birthday',
+                v::stringType()->date()
+            )->key(
+                "username", 
+                v::stringType()->length(3, 60)
+            );
             
-            $email = $_POST["email"];
-            $name = $_POST["name"];
-            $date_birthday = $_POST["date_birthday"];
-            $password = $_POST["password"];
-            $password_confirmation = $_POST["password_confirmation"];
-            $token_recaptcha = $_POST["token_recaptcha"];
+            $schema->assert($_POST);
 
-            $res = verifyRecaptcha($token_recaptcha);
-        
-            if(!$res) {
-                http_response_code(400);
-                return [
-                    "success" => false,
-                    "errors" => [
-                        "error" => true,
-                        "recaptcha" => [
-                            "error_code" => 1,
-                            "message" => "Recaptcha inválido"
-                        ]
-                    ]
-                ];
-            }
-        
-            $errors = [
-                "error" => false,
-                "email" => [
-                    "error_code" => 0,
-                    "message" => ""
-                ],
-                "name" => [
-                    "error_code" => 0,
-                    "message" => ""
-                ],
-                "date_birthday" => [
-                    "error_code" => 0,
-                    "message" => ""
-                ],
-                "password" => [
-                    "error_code" => 0,
-                    "message" => ""
-                ],
-                "password_confirmation" => [
-                    "error_code" => 0,
-                    "message" => ""
-                ]
-            ];
-        
-            if($password !== $password_confirmation) {
-                http_response_code(400);
-                
-                $errors["error"] = true;
-                $errors["password_confirmation"]["error_code"] = 1;
-                $errors["password_confirmation"]["message"] = "As senhas não coincidem";
-            }
-        
-            if(strlen($password) < 6) {
-                http_response_code(400);
-        
-                $errors["error"] = true;
-                $errors["password"]["error_code"] = 1;
-                $errors["password"]["message"] = "A senha deve ter no mínimo 6 caracteres";
-            }
-        
-            if(strlen($password) > 12) {
-                http_response_code(400);
-               
-                $errors["error"] = true;
-                $errors["password"]["error_code"] = 1;
-                $errors["password"]["message"] = "A senha deve ter no máximo 12 caracteres";
-            }
-        
-            if(strlen($name) < 3) {
-                http_response_code(400);
-               
-                $errors["error"] = true;
-                $errors["name"]["error_code"] = 1;
-                $errors["name"]["message"] = "O nome deve ter no mínimo 3 caracteres";
-            }
-        
-            if(strlen($name) > 255) {
-                http_response_code(400);
-               
-                $errors["error"] = true;
-                $errors["name"]["error_code"] = 1;
-                $errors["name"]["message"] = "O nome deve ter no máximo 255 caracteres";
-            }
-        
-            if(filter_var($email, FILTER_VALIDATE_EMAIL) === false) {
-                http_response_code(400);
-                
-                $errors["error"] = true;
-                $errors["email"]["error_code"] = 1;
-                $errors["email"]["message"] = "Email inválido";
-            }
+            $_POST["password"] = password_hash($_POST["password"], PASSWORD_BCRYPT, [
+                "cost" => 14
+            ]);
 
-            if(filter_var($date_birthday, FILTER_VALIDATE_REGEXP, ["options" => ["regexp" => "/\d{4}-\d{2}-\d{2}/"]]) === false) {
-                http_response_code(400);
-                
-                $errors["error"] = true;
-                $errors["date_birthday"]["error_code"] = 1;
-                $errors["date_birthday"]["message"] = "Data de nascimento inválida";
-            }
+            $this->userModel->create($_POST["name"], $_POST["email"], $_POST["password"], $_POST["username"], $_POST["date_birthday"]);
+        } catch (NestedValidationException $exception) {
+            echo $exception->getFullMessage();
+        }
         
-            if($errors['error']) {
-                return [
-                    "success" => false,
-                    "errors" => $errors
-                ];
-            }
-        
-        
-            $userModel = new UserModel();
-
-            $user = $userModel->findByEmail($email);
-
-            if($user) {
-                http_response_code(400);
-                return [
-                    "success" => false,
-                    "errors" => [
-                        "error" => true,
-                        "email" => [
-                            "error_code" => 1,
-                            "message" => "Email já cadastrado"
-                        ]
-                    ]
-                ];
-            }
-        
-            $userModel->create($name, $email, password_hash($password, PASSWORD_BCRYPT), $email, $date_birthday);
-            
-            $emailTransporter = new EmailTransporter();
-        
-            $file_path = __DIR__ . '/../application/utils/emails/createAccountEmail.html';
-        
-            $emailHTML = fopen($file_path, "r");
-            $emailHTML = fread($emailHTML, filesize($file_path));
-            $emailHTML = str_replace("[Nome do Usuário]", $name, $emailHTML);
-        
-            $emailTransporter->sendEmail($email, $name, "Bem-vindo ao nosso sistema", $emailHTML);
-        
-            return [
-                "success" => true,
-                "message" => "Usuário criado com sucesso, verifique seu email para ativar sua conta"
-            ];
-            
-        } catch (\Throwable $th) {
-            http_response_code(500);
-            return [
-                "success" => false,
-                "errors" => [
-                    "error" => true,
-                    "message" => "Erro interno no servidor"
-                ]
-            ];
-        }   
-
     }
 }
