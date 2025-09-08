b15656dedb789f8d78935b0d422b35b070743943
Guilherme Freitas do Nascimento <121752740+SrFreitass@users.noreply.github.com>
2025-09-08 15:17:50 -0400

    chore: suporte de até 2 pastas para import automático de controller

--- src/vendor/routes.autoload.php
+++ src/vendor/routes.autoload.php
@@ -3,8 +3,17 @@
 foreach (new DirectoryIterator(__DIR__ . '/../controllers') as $file) {
     if($file->isDir() && !$file->isDot()) {
         $filePath = $file->getPathname();
-        
+
         foreach (new DirectoryIterator($filePath) as $subFile) {
+            if($subFile->isDir() && !$subFile->isDot()) {
+                // Até 2 pastas...
+                foreach(new DirectoryIterator($filePath) as $subFile) {
+                    if($subFile->getExtension() === 'php' && str_contains($subFile->getFilename(), '.controller.php')) {
+                        require_once $subFile->getPathname();
+                    }
+                }
+            }
+
             if($subFile->getExtension() === 'php' && str_contains($subFile->getFilename(), '.controller.php')) {
                 require_once $subFile->getPathname();
             }
