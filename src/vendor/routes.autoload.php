825c82aca8d90d5ff29972b0b69cb07d326133f0
Guilherme Freitas do Nascimento <121752740+SrFreitass@users.noreply.github.com>
2025-09-08 16:08:43 -0400

    fix: ultima commit, /controller/$1/$2/arquivo

--- src/vendor/routes.autoload.php
+++ src/vendor/routes.autoload.php
@@ -8,6 +8,13 @@ foreach (new DirectoryIterator(__DIR__ . '/../controllers') as $file) {
             if($subFile->isDir() && !$subFile->isDot()) {
                 // Até 2 pastas...
                 foreach(new DirectoryIterator($filePath) as $subFile) {
+
+                    foreach(new DirectoryIterator($subFile->getPathname()) as $subFile) {
+                        if($subFile->getExtension() === 'php' && str_contains($subFile->getFilename(), '.controller.php')) {
+                            require_once $subFile->getPathname();
+                        }    
+                    }
+
                     if($subFile->getExtension() === 'php' && str_contains($subFile->getFilename(), '.controller.php')) {
                         require_once $subFile->getPathname();
                     }
