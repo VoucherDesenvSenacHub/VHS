43e4d3dcbce19335656a28e306bcb5d4a7d80e0f
____x_ <josuealexandre067@gmail.com>
2025-09-12 11:31:04 -0400

    ref: sidebar, header, events (page) #107
    * ref/tirar gradiente e melhorando a cor dos icones ativos
    
    * ref/excluir arquivo para alterar seu nome para index
    
    * feat/remover arquivo depreciado e adicionar sidebar melhorada
    
    * feat/melhorar animação dos titles
    
    * ref/importar sidebar refatorado pelo novo nome
    
    * ref/novo header
    
    * ref/trazer melhorias no componente FeaturedEvent para receber dados do middleware futuramente

--- src/views/pages/home/events/index.php
+++ src/views/pages/home/events/index.php
@@ -1,15 +1,14 @@
 <?php
 
     require_once __DIR__ . "/../../../components/header/headerComponent.php";
-    require_once __DIR__ . "/../../../components/sidebar/SidebarComponent.php";
-    require_once __DIR__ . "/../../../components/cards/index.php";
+    require_once __DIR__ . "/../../../components/sidebar/index.php";
     require_once __DIR__ . "/../../../components/featuredCard/featuredEventComponent.php";
     require_once __DIR__ . "/../../../components/cards/index.php";
     
-    use function Src\Views\Components\sidebar\SidebarComponent;
-    use function Src\Views\Components\header\HeaderComponent;
-    use function Src\Views\Components\Cards\renderCards;
-    use function Views\Components\FeaturedCard\FeaturedEventCard;
+    use function Src\Views\Components\Sidebar\SidebarComponent;
+    use function Src\Views\Components\Header\HeaderComponent;
+    use function Src\Views\Components\Cards\viewCards;
+    use function Src\Views\Components\FeaturedCard\FeaturedEventCard;
 
     $category = isset($_GET['category']) ? $_GET['category'] : 'tecnologia';
 
@@ -21,141 +20,75 @@
     ];
 
     $featuredVieo = [
-        'image_url' => 'https://framerusercontent.com/images/TO1bOWR2ihsAvIgtbf5Y9taYWZs.png',
+        'thumbnail_url' => 'https://framerusercontent.com/images/TO1bOWR2ihsAvIgtbf5Y9taYWZs.png',
         'title' => 'Usando IA para processamento de geoinformação em Python, com GIS AXIS',
-        'instructor' => 'Ministrado por Paulo Silveira',
-        'event_type' => 'Pago | Presencial',
-        'date' => '09-08 18:15',
+        'name' => 'Paulo Silveira',
+        'description' => 'Gratuito',
+        'event_date' => '12-09-2025 09:55:00',
     ];
 
     $techVideos = [
         [
-            "url" => "https://youtube.com/watch?v=nextjs4",
-            "type_card" => "event",
-            "description" => "Rafael Germano",
-            "duration" => "7 min",
+            "url" => "#",
+            "description" => "Gratuito",
             "title" => "Tudo sobre o Next.js 15, nova arquitetura de pasta",
-            "username" => "Rafael Germano",
+            "name" => "Rafael Germano",
             "thumbnail_url" => "https://framerusercontent.com/images/TO1bOWR2ihsAvIgtbf5Y9taYWZs.png", 
-            "avatar_url" => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTS2EntOCdE0yEaIfacfxiU1ZyRi8RSeT-eu_HDeQSq6J_veZZesXpwlcxkWxM2NKMpWRb4CRyw9WdUGOQV7ZqK8g",
-            "views" => "8.5k views",
-            "created_at" => "há 2 semanas atrás",
-            "maked_for" => "Online",
-            "likes" => 750,
-            "comments" => 45,
-            "event_date" => "09/08 às 18:15"
-        ],
-        [
-            "url" => "https://youtube.com/watch?v=python2",
-            "type_card" => "event",
-            "description" => "Rafael Germano",
-            "duration" => "7 min",
-            "title" => "Aprenda PYTHON em 1 hora",
-            "username" => "Rafael Germano",
-            "thumbnail_url" => "https://framerusercontent.com/images/TO1bOWR2ihsAvIgtbf5Y9taYWZs.png",
-            "avatar_url" => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTS2EntOCdE0yEaIfacfxiU1ZyRi8RSeT-eu_HDeQSq6J_veZZesXpwlcxkWxM2NKMpWRb4CRyw9WdUGOQV7ZqK8g", 
-            "views" => "8.5k views",
-            "created_at" => "há 2 semanas atrás",
-            "maked_for" => "Online",
-            "likes" => 750,
-            "comments" => 45,
-            "event_date" => "09/08 às 18:15"
-        ],
-        [
-            "url" => "https://youtube.com/watch?v=nextjs4",
-            "type_card" => "event",
-            "description" => "Rafael Germano",
-            "duration" => "7 min",
-            "title" => "Tudo sobre o Next.js 15, nova arquitetura de pasta",
-            "username" => "Rafael Germano",
-            "thumbnail_url" => "https://framerusercontent.com/images/TO1bOWR2ihsAvIgtbf5Y9taYWZs.png", 
-            "avatar_url" => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTS2EntOCdE0yEaIfacfxiU1ZyRi8RSeT-eu_HDeQSq6J_veZZesXpwlcxkWxM2NKMpWRb4CRyw9WdUGOQV7ZqK8g",
-            "views" => "8.5k views",
-            "created_at" => "há 2 semanas atrás",
-            "maked_for" => "Online",
-            "likes" => 750,
-            "comments" => 45,
-            "event_date" => "09/08 às 18:15",
-        ],
-        [
-            "url" => "https://youtube.com/watch?v=python2",
-            "type_card" => "event",
-            "description" => "Rafael Germano",
-            "duration" => "7 min",
-            "title" => "Aprenda PYTHON em 1 hora",
-            "username" => "Rafael Germano",
-            "thumbnail_url" => "https://framerusercontent.com/images/TO1bOWR2ihsAvIgtbf5Y9taYWZs.png",
-            "avatar_url" => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTS2EntOCdE0yEaIfacfxiU1ZyRi8RSeT-eu_HDeQSq6J_veZZesXpwlcxkWxM2NKMpWRb4CRyw9WdUGOQV7ZqK8g", 
-            "views" => "8.5k views",
-            "created_at" => "há 2 semanas atrás",
-            "maked_for" => "Online",
-            "likes" => 750,
-            "comments" => 45,
-            "event_date" => "09/08 às 18:15"
-        ],
-        [
-            "url" => "https://youtube.com/watch?v=python2",
-            "type_card" => "event",
-            "description" => "Rafael Germano",
-            "duration" => "7 min",
-            "title" => "Aprenda PYTHON em 1 hora",
-            "username" => "Rafael Germano",
-            "thumbnail_url" => "https://framerusercontent.com/images/TO1bOWR2ihsAvIgtbf5Y9taYWZs.png",
-            "avatar_url" => "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTS2EntOCdE0yEaIfacfxiU1ZyRi8RSeT-eu_HDeQSq6J_veZZesXpwlcxkWxM2NKMpWRb4CRyw9WdUGOQV7ZqK8g", 
-            "views" => "8.5k views",
-            "created_at" => "há 2 semanas atrás",
-            "maked_for" => "Online",
-            "likes" => 750,
-            "comments" => 45,
-            "event_date" => "09/08 às 18:15"
+            "event_date" => "2025-09-15 10:00"
         ]
     ];
 
 ?>
+
+<!-- H T M L -->
 
 <!DOCTYPE html>
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>VHS - Evento</title>
+    <title>VHS - Eventos</title>
+
     <link rel="stylesheet" href="/VHS/src/styles/global.css">
     <script src="https://cdn.tailwindcss.com"></script>
     <script type="module" src="/VHS/src/styles/tailwindglobal.js"></script>
-    <link rel="stylesheet" href="/VHS/src/styles/global.css">
 </head>
 
-<body class="w-full min-h-screen bg-background">
-    <?= HeaderComponent(); ?>
+<body>
+
+    <?= HeaderComponent() ?>
 
     <div class="flex flex-col md:flex-row w-full">
-        <?= SidebarComponent(); ?>
+        <?= SidebarComponent() ?>
 
-        <main class="flex-1 px-4 sm:px-6 py-4 mx-auto">
+        <main class="flex-1 p-6 mx-auto">
             <div class="max-w-[1500px] mx-auto">
                 <div>
-                    <h2 class="text-2xl font-bold text-white mb-2"><span class="text-purple-400">#</span> Eventos  🚀</h2>
-                    <p class="text-gray-400 text-sm mb-6">Confira o evento que está acontecendo agora 🔥</p>
+                    <h2 class="text-2xl font-bold text-white"><span class="text-purple-400">#</span> Acontecendo agora 🚀</h2>
+                    <p class="text-gray-400 text-sm mb-6">Recomendado para você</p>
                 </div>
-                
-                
+
                 <section class="mb-12">
                     <div class="grid grid-cols-1 gap-6">
                         <div class="lg:col-span-1">
-                            <?= FeaturedEventCard($featuredVieo, true)  ?>
+                            <?= FeaturedEventCard($featuredVieo, true) ?>
                         </div>
+                    </div>
                 </section>
+
                 <section class="mb-12">
                     <div>
-                        <h2 class="text-2xl font-bold text-white mb-2"><span class="text-purple-400">#</span> Eventos que irão acontecer 🔥</h2>
-                        <p class="text-gray-400 text-sm mb-6">Confira os vídeo mais populares da nossa plataforma VHS</p>
+                        <h2 class="text-2xl font-bold text-white"><span class="text-purple-400">#</span> Daqui a pouco 🔥</h2>
+                        <p class="text-gray-400 text-sm mb-6">Confira os eventos mais populares do VHS</p>
                     </div>
+
                     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
-                        <?= renderCards($techVideos, 'event'); ?>   
-                        </div>
-                    </section>
+                        <?= viewCards($techVideos, 'events'); ?>   
+                    </div>
+                </section>
             </div>
         </main>
     </div>
+
 </body>
 </html>
