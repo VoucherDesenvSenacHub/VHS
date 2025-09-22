049819842250d97725cbe56163f929c4f969b1a6
Guilherme Freitas do Nascimento <121752740+SrFreitass@users.noreply.github.com>
2025-09-22 15:16:57 -0400

    refactor: aperfeiçoamento dos controllers de auth, sign, sign-up e middlewares

--- src/application/middlewares/RedirectUserNotCreator.middleware.php
+++ src/application/middlewares/RedirectUserNotCreator.middleware.php
@@ -15,8 +15,14 @@ use function Src\Application\Utils\Redirect\redirect;
 
 class RedirectUserNotCreatorMiddleware {
     public function execute() {
-        if($_SESSION["user"]["role"] == "USER") {
-            return redirect($_SERVER['HTTP_REFERER'] ?? "/VHS/home", ["errors" => "Você não tem permissão para executar essa ação."]);
+        if(empty($_SESSION["user"])) {
+            http_response_code(401);
+            return redirect("/VHS/home");
+        }
+
+        if($_SESSION["user"]["role"] === "USER") {
+            http_response_code(403);
+            return redirect("/VHS/home");
         }
     }
 }
