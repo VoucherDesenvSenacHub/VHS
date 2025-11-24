6278598bb916d305724119e95fb1dea374071348
Ronan <ronanscampos@hotmail.com>
2025-11-24 16:23:59 -0400

    fix: update default items per page in paginate function from 10 to 7

--- src/application/utils/pagination.php
+++ src/application/utils/pagination.php
@@ -9,7 +9,7 @@ namespace Src\Application\Utils;
  * @param array $content Array de itens a serem paginados
  * @return string HTML completo da paginação
  */
-function paginate($content, $totalPerPage = 10) {
+function paginate($content, $totalPerPage = 7) {
     $page = isset($_GET['page']) ? (int)$_GET['page'] : 0;
     if ($page < 1) $page = 0;
     $prevPage = $page > 1 ? $page - 1 : 0;
