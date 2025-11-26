cd94bf8aebf7c299ba7df198e6ea22acf937099b
HenriqueToyota <henriquetoyota13@gmail.com>
2025-11-26 15:00:25 -0400

    fix: ajustando erro no input

--- src/views/pages/user/settings/index.php
+++ src/views/pages/user/settings/index.php
@@ -43,53 +43,41 @@ $avatar_url = !empty($user['avatar_url']) ? $user['avatar_url'] : 'default.png';
             userCategories.push("<?= $category["name"] ?>");
         <?php endforeach; ?>
     </script>
-
     <div>
         <?= HeaderComponent() ?>
     </div>
-
     <div class="flex flex-col md:flex-row w-full">
         <div class="hidden md:block">
             <?= SidebarComponent() ?>
         </div>
-
         <div class="flex flex-col gap-4 p-6 w-full max-w-[1200px] mx-auto">
-           <div class="w-full">
-            <h2 class="text-xl md:text-3xl font-bold text-white text-center md:text-left">Gerenciar Perfil</h2>
-        </div>
-
-            <form class="flex flex-col gap-10 rounded-xl w-full" 
-                  method="post" 
-                  action="/VHS/api/v1/user/settings" 
-                  enctype="multipart/form-data" 
-                  id="form">
-
-                <!-- FOTO -->
+            <div class="w-full">
+                <h2 class="text-xl md:text-3xl font-bold text-white text-center md:text-left">Gerenciar Perfil</h2>
+            </div>
+            <form class="flex flex-col gap-10 rounded-xl w-full"
+                method="post"
+                action="/VHS/api/v1/user/settings"
+                enctype="multipart/form-data"
+                id="form">
                 <div class="flex flex-col md:flex-row items-center gap-4 w-full">
                     <div class="w-36 h-36 relative overflow-hidden rounded-full">
                         <img id="profileImage" src="/VHS/public/uploads/avatars/<?= $avatar_url ?>" class="object-cover w-full h-full">
                     </div>
-
                     <div class="flex space-x-3 w-full md:w-[28rem]">
                         <button id="uploadButton"
                             class="bg-purple-700 hover:bg-purple-800 text-white rounded w-full h-[40px] md:h-[48px] text-sm md:text-base"
                             type="button">
                             Carregar foto
                         </button>
-
                         <button id="deleteButton"
                             class="w-full h-[40px] md:h-[48px] outline outline-1 outline-purple-500 text-white rounded text-sm md:text-base"
                             type="button">
                             Excluir
                         </button>
                     </div>
-
                     <input type="file" name="avatar" id="imageUpload" accept="image/*" class="hidden">
                 </div>
-
-                <!-- INPUTS -->
                 <div class="flex flex-col gap-6 w-full ">
-
                     <?= InputComponent(
                         placeholder: "@UsuárioSenac12333",
                         name: "name",
@@ -97,45 +85,38 @@ $avatar_url = !empty($user['avatar_url']) ? $user['avatar_url'] : 'default.png';
                         label: "Nome de Usuário",
                         value: $user["name"],
                     ) ?>
-
                     <?= InputComponent(
                         placeholder: "@UsuárioSenac12333",
                         name: "username",
                         type: "text",
                         label: "Usuário",
                         value: $user["username"],
-                        error: $errors["username"] ?? null,
-                        errorDescription: $errors["username"] ?? null,
+                        error: $errors["username"] ?? "",
+                        errorDescription: $errors["username"] ?? "",
                     ) ?>
-
                     <?= InputComponent(
                         placeholder: "Usuario123@gmail.com",
                         name: "email",
                         type: "email",
                         label: "E-mail",
                         value: $user["email"],
-                        error: isset($errors["email"]) ? true : null,
-                        errorDescription: $errors["email"] ?? null,
+                        error: isset($errors["email"]) ? true : "",
+                        errorDescription: $errors["email"] ?? "",
                     ) ?>
-
                     <?= InputComponent(
                         placeholder: "Sua senha atual",
                         name: "password",
                         type: "password",
                         label: "Senha",
                     ) ?>
-
                     <?= InputComponent(
                         placeholder: "Sua nova senha",
                         name: "new_password",
                         type: "password",
                         label: "Nova senha",
                     ) ?>
-
-                    <!-- CATEGORIAS -->
                     <div class="flex flex-col gap-2">
                         <label class="text-gray-300 font-medium">Categorias que você se interessa</label>
-
                         <div id="categoryButtons" class="flex flex-wrap gap-2">
                             <?php foreach ($categories as $category): ?>
                                 <button type="button"
@@ -144,27 +125,24 @@ $avatar_url = !empty($user['avatar_url']) ? $user['avatar_url'] : 'default.png';
                                     <?= $category['name'] ?>
                                 </button>
                             <?php endforeach; ?>
-
                             <input type="text" hidden name="categories[]" id="categoriesInput">
                         </div>
                     </div>
                 </div>
-
-                <!-- BOTÕES FINAIS -->
                 <div class="flex items-center justify-between w-full md:w-[28rem] self-end gap-4">
                     <?= ButtonComponent("Cancelar", "outline", null, type: "button"); ?>
                     <?= ButtonComponent("Salvar", "default", null, type: "submit"); ?>
                 </div>
             </form>
         </div>
-
-        <?php 
+        <?php
         if ($errors) {
             echo "<script>alert('Existem erros no formulário, por favor verifique os campos!');</script>";
         }
         ?>
     </div>
 </body>
+
 </html>
 
 <script>
