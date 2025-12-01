fd0a04a298019984ee2ee742552471f64be7b6e8
zuiaas <josuealexandre067@gmail.com>
2025-12-01 00:59:43 -0400

    ref: retirar var_dump() em redirect.php, seguindo fluxo normal

--- src/application/utils/redirect.php
+++ src/application/utils/redirect.php
@@ -1,21 +1,21 @@
 <?php
+
 namespace Src\Application\Utils\Redirect;
+
 /**
  * Summary of redirect
  * @param string $route Caminho da rota de redirecionamento
  * @param array|null $data Salvando na sessão [redirect_data], quando salva mais de uma vez a anterior é substituida
  * @return void
  */
-function redirect(string $route, array | null $data = null, int $status_code = 302): void {
-    if(session_status() == 0) {
-        session_start();
-    }
-
-    if($data) {
-        echo var_dump($data);
-        $_SESSION["redirect_data"] = $data;
-    };
+function redirect (
+    string $route,
+    array | null $data = null,
+    int $status_code = 302
+) {
+    
+    if (session_status() == 0) session_start();
+    if ($data) $_SESSION["redirect_data"] = $data;
 
     header("Location: $route", true, $status_code);
 }
-
