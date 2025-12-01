6d3cbd0d94e6e74ac653706825fc93b0172269f9
zuiaas <josuealexandre067@gmail.com>
2025-12-01 01:32:27 -0400

    feat: modificar modal do @montealvares para o usuário ser corretamente redirecionado após criar conteúdo

--- src/views/components/modal/modal.component.php
+++ src/views/components/modal/modal.component.php
@@ -6,18 +6,30 @@ require_once __DIR__ . '/../utils/buttonComponent.php';
 
 use function Src\Views\Components\Utils\ButtonComponent;
 
-function ModalComponent(string $title, string $description)
-{
+function ModalComponent (
+    string $title,
+    string $description,
+    string $cancelLink,
+    string $confirmLink
+) {
+
     return <<<HTML
-        <div class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-            <div class="border border-white rounded-xl shadow-lg p-6 w-[400px] text-white">
-                <h2 class="text-xl font-bold mb-2 text-center">$title</h2>
-                <p class="mb-4">$description</p>
+        <style>
+            body {
+                overflow: hidden;
+            }
+        </style>
+
+        <div class="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
+            <div class="border border-[#666] rounded-xl shadow-lg p-6 w-[400px] text-white bg-gray600">
+                <h2 class="text-xl font-bold">$title</h2>
+                <p class="mb-6 text-[#666]">$description</p>
                 <div class="flex justify-end gap-4">
     HTML
-        . ButtonComponent(text: "Cancelar", variant: "outline", className: "btn-cancel w-[10.675rem] h-[2.5rem]", link: '/VHS/home')
-        . ButtonComponent(text: "Confimar", variant: "default", className: "btn-confirm w-[10.675rem] h-[2.5rem]", link: '/VHS/studio/create/video')
-        . <<<HTML
+                    . ButtonComponent(text: "Agora não", variant: "outline", className: "btn-cancel w-[10.675rem] h-[2.5rem] outline-[#666]", link: $cancelLink)
+                    . ButtonComponent(text: "Sim", variant: "default", className: "btn-confirm w-[10.675rem] h-[2.5rem]", link: $confirmLink)
+                    .
+    <<<HTML
                 </div>
             </div>
         </div>
