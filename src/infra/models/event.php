f68adcfdf4c5d9d18b1470ac1dca99dfdcc94b40
zuiaas <josuealexandre067@gmail.com>
2025-12-01 12:23:45 -0400

    feat: adicionar novas consultas ao model

--- src/infra/models/event.php
+++ src/infra/models/event.php
@@ -45,20 +45,87 @@ class EventModel extends Model {
                 events.id,
                 users.name,
                 events.url,
-                events.thumbnail_url,
                 events.title,
-                categories.name as category,
-                events.event_date
+                categories.name AS category,
+                events.event_date,
+                events.thumbnail_url
             FROM events
                 INNER JOIN users ON events.author_id = users.id
                 INNER JOIN categories ON events.category_id = categories.id
-            WHERE events.is_deleted = 0;
+            WHERE events.is_deleted = 0
+                AND events.event_date >= NOW()
+                ORDER BY events.event_date ASC;
         SQL;
 
         return $this->database->query($sql, []);
     }
 
     # Outras Consultas
+
+    public function getEventById(string $id) {
+        $sql = <<<SQL
+            SELECT 
+                events.*,
+                users.name AS author_name,
+                categories.name AS category_name
+            FROM events
+                LEFT JOIN users ON users.id = events.author_id
+                LEFT JOIN categories ON categories.id = events.category_id
+            WHERE 
+                events.is_deleted = 0
+                AND events.id = :id
+            LIMIT 1
+        SQL;
+
+        return $this->database->query($sql, [":id" => $id]);
+    }
+
+    public function getPopularEvents() {
+        $sql = <<<SQL
+            SELECT
+                events.id,
+                users.name,
+                events.url,
+                events.title,
+                categories.name as category,
+                events.event_date,
+                events.thumbnail_url
+            FROM events
+                INNER JOIN users
+                    ON events.author_id = users.id
+                INNER JOIN categories
+                    ON events.category_id = categories.id
+                ORDER BY events.views DESC
+                    LIMIT 8;
+        SQL;
+
+        return $this->database->query($sql, []);
+    }
+
+    public function getNextEvent() {
+        $sql = <<<SQL
+            SELECT
+                events.id,
+                users.name AS author_name,
+                events.url,
+                events.title,
+                categories.name AS category,
+                events.event_date,
+                events.thumbnail_url
+            FROM events
+                INNER JOIN users
+                    ON events.author_id = users.id
+                INNER JOIN categories
+                    ON events.category_id = categories.id
+            WHERE 
+                events.is_deleted = 0
+                AND events.event_date >= NOW()
+            ORDER BY events.event_date ASC
+            LIMIT 1
+        SQL;
+
+        return $this->database->query($sql, []);
+    }
 
     public function getEventsByTitle(string $query): array {
         $sql = <<<SQL
