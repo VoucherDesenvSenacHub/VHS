a2c684568a0c539834bd9daa0729891e89d38d2b
zuiaas <josuealexandre067@gmail.com>
2025-12-01 01:02:54 -0400

    feat: puxar novo UploadImages de @Montealvao 🙏

--- src/application/utils/uploadImages.php
+++ src/application/utils/uploadImages.php
@@ -2,8 +2,12 @@
 
 namespace Src\Application\Utils;
 
-function UploadImages(string $file_name, string $user ,bool $isAvatar = false)
-{
+function UploadImages (
+    string $file_name,
+    string $user,
+    bool $isAvatar = false,
+    string | null $oldPath = null
+) {
 
     $user = str_replace(" ", "_", $user);
     $user = mb_strtolower($user, "UTF-8");
@@ -17,40 +21,36 @@ function UploadImages(string $file_name, string $user ,bool $isAvatar = false)
     }
 
     $file = $_FILES[$file_name];
-
     if ($file["error"] !== UPLOAD_ERR_OK) {
         throw new \Exception("Erro no upload: " . $file["error"]);
     }
 
-    $origin_name = $file["name"];
-    $type        = $file["type"];
-    $size        = $file["size"];
-    $tmp_name    = $file["tmp_name"];
-
     $allowedTypes = ["image/jpeg", "image/png", "image/jpg"];
-    if (!in_array($type, $allowedTypes)) {
+    if (!in_array($file["type"], $allowedTypes)) {
         throw new \Exception("Tipo de arquivo não permitido");
     }
 
     $maxSize = 2 * 1024 * 1024;
-    if ($size > $maxSize) {
+    if ($file["size"] > $maxSize) {
         throw new \Exception("Arquivo muito grande");
     }
 
-    $escolha = "thumbs";
-
-    $isAvatar = $isAvatar ? $escolha = "avatars" : $escolha;
-
+    $escolha = $isAvatar ? "avatars" : "thumbnails";
     $dir = __DIR__ . "/../../../public/uploads/$escolha/$user/";
 
     if (!is_dir($dir)) {
         mkdir($dir, 0777, true);
     }
-    
-    $safeName = time() . "-" . basename($origin_name);
+
+    if ($oldPath) {
+        $oldFullPath = __DIR__ . "/../../../public" . str_replace("/VHS/public", "", $oldPath);
+        if (file_exists($oldFullPath)) unlink($oldFullPath);
+    }
+
+    $safeName = time() . "-" . basename($file["name"]);
     $path = $dir . $safeName;
 
-    if (!move_uploaded_file($tmp_name, $path)) {
+    if (!move_uploaded_file($file["tmp_name"], $path)) {
         throw new \Exception("Falha ao salvar o arquivo");
     }
 
